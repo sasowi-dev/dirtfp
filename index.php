0cf9dfe7c54174bdfc5b52fa3cafdc5515dd5da1
Leek <redev@redevs.ga>
2021-01-24 02:18:37 +0900

    Update index.php

--- index.php
+++ index.php
@@ -5,11 +5,11 @@
   </head>
   <body>
 <?php include 'header.php';?>
-<img src="svg/adless_switch.svg">
-<img src="svg/adless_switch_pg2.svg">
-<img src="svg/adless_switch_pg3.svg">
+<img src="svg/free1.svg">
+<img src="svg/free2.svg">
+<img src="svg/free3.svg">
 <a href="http://open.kakao.com/me/leekryty">
-<img src="svg/adless_switch_pg4.svg">
+<img src="svg/free4.svg">
 </a>
 <?php include 'footer.php';?>
 <!-- Start of LiveChat (www.livechatinc.com) code -->
