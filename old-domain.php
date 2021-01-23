cd56c00c9aea50290ecf34b14e664451c2f3c932
Leek <redev@redevs.ga>
2021-01-23 15:19:36 +0900

    Update old-domain.php

--- old-domain.php
+++ old-domain.php
@@ -5,7 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="preconnect" href="https://fonts.gstatic.com">
     <link href="https://fonts.googleapis.com/css2?family=Noto+Sans+KR:wght@100;300;400;500;700;900&display=swap" rel="stylesheet">
-    <title>테스트!!</title>
+    <title>테스트!!sdfdf</title>
     <script>
     </script>
     <style>
