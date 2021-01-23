169623a8b2799efd5d7c1b4f763925781862b5af
Leek <redev@redevs.ga>
2021-01-23 15:15:39 +0900

    Update gitpull.php

--- gitpull.php
+++ gitpull.php
@@ -3,7 +3,7 @@
 // Use in the “Post-Receive URLs” section of your GitHub repo.
 
 if ( $_POST['payload'] ) {
-shell_exec('cd /home/pi/withunlink-pi && sudo git reset –hard HEAD && sudo git pull');
+shell_exec('cd /home/pi/withunlink-pi && sudo git pull');
 }
 
 ?>
