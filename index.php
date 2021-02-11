bb7fbf3a39c8144f8666bdacf135570a83734273
Leek <redev@redevs.ga>
2021-02-11 23:41:07 +0900

    Update index.php

--- index.php
+++ index.php
@@ -4,16 +4,19 @@
 <?php include 'dependencies.php';?>
   </head>
   <body>
-<?php include 'evemt.php';?>
+<?php include 'event.php';?>
 <?php include 'header.php';?>
 <div class="lg:2/6 xl:w-2/4 mt-20 lg:mt-40 lg:ml-16 text-left">
             <div class="text-6xl font-semibold text-gray-900 leading-none">
-  <div class="mr-2 py-3 px-4 bg-blue-400 rounded-bl-3xl rounded-tl-3xl rounded-tr-xl text-white">
-             메시징의 변화를, 삶의 변화를.
+  <div class="mr-2 py-3 px-4 bg-indigo-700 rounded-bl-3xl rounded-tl-3xl rounded-tr-xl text-white">
+             메시징의 변화를,
+            </div>
+  <div class="ml-2 py-3 px-4 bg-indigo-300 rounded-br-3xl rounded-tr-3xl rounded-tl-xl text-white">
+              삶의 변화를.
             </div>
   </div>
             <div class="mt-6 text-xl font-light text-true-gray-500 antialiased">A better experience for yout attendees and less stress yout team.</div>
-            <button class="mt-6 px-8 py-4 rounded-full font-normal tracking-wide bg-gradient-to-b from-blue-600 to-blue-700 text-white outline-none focus:outline-none hover:shadow-lg hover:from-blue-700 transition duration-200 ease-in-out">
+            <button class="mt-6 px-8 py-4 rounded-full font-normal tracking-wide bg-indigo-700 text-white outline-none focus:outline-none hover:shadow-lg hover:from-blue-700 transition duration-200 ease-in-out">
               Download for Free
             </button>
           </div>
