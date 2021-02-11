1aec60c9ac544ce675e87af914fbe2bafc2be31b
Leek <redev@redevs.ga>
2021-02-11 22:22:01 +0900

    Update and rename index-old2.php to index.php

---
+++ index.php
@@ -0,0 +1,13 @@
+<html lang="ko">
+<head>
+<meta charset="utf-8">
+<?php include 'dependencies.php';?>
+  </head>
+  <body>
+<?php include 'evemt.php';?>
+<?php include 'header.php';?>
+설에는마음을담으세요정관장<br>
+    테스트 ㅅㄱ
+<?php include 'footer.php';?>
+</body>
+</html>
