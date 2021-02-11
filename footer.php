00b4edd1b324b29502807f10834eecd8138359f3
Leek <redev@redevs.ga>
2021-02-11 22:59:06 +0900

    Update footer.php

--- footer.php
+++ footer.php
@@ -1,91 +1,48 @@
 <footer class="bg-gray-800 pt-10 sm:mt-10 pt-10">
-    <div class="max-w-6xl m-auto text-gray-800 flex flex-wrap justify-left">
-        <!-- Col-1 -->
-        <div class="p-5 w-1/2 sm:w-4/12 md:w-3/12">
-            <!-- Col Title -->
-            <div class="text-xs uppercase text-gray-400 font-medium mb-6">사이트맵</div>
+<div class="max-w-6xl m-auto text-gray-800 flex flex-wrap justify-left">
 
-            <!-- Links -->
-            <a href="#" class="my-3 block text-gray-300 hover:text-gray-100 text-sm font-medium duration-700">리크봇</a>
-            <a href="#" class="my-3 block text-gray-300 hover:text-gray-100 text-sm font-medium duration-700">모임챗</a>
-            <a href="#" class="my-3 block text-gray-300 hover:text-gray-100 text-sm font-medium duration-700">개발자를 위하여</a>
-            <a href="#" class="my-3 block text-gray-300 hover:text-gray-100 text-sm font-medium duration-700">광고</a>
-            <a href="#" class="my-3 block text-gray-300 hover:text-gray-100 text-sm font-medium duration-700">비즈니스</a>
-            
-            
-        </div>
+<div class="p-5 w-1/2 sm:w-4/12 md:w-3/12">
 
-        <!-- Col-2 -->
-        <div class="p-5 w-1/2 sm:w-4/12 md:w-3/12">
-            <!-- Col Title -->
-            <div class="text-xs uppercase text-gray-400 font-medium mb-6">개인정보 센터</div>
+<div class="text-xs uppercase text-gray-400 font-medium mb-6">페이퍼코어 패밀리</div>
 
-            <!-- Links -->
-            <a href="#" class="my-3 block text-gray-300 hover:text-gray-100 text-sm font-medium duration-700">개인정보 센터</a>
-            <a href="#" class="my-3 block text-gray-300 hover:text-gray-100 text-sm font-medium duration-700">이용 약관</a>
-            <a href="#" class="my-3 block text-gray-300 hover:text-gray-100 text-sm font-medium duration-700">개인정보처리방침</a>
-            
-            
-            
-            
-        </div>
+<a href="#" class="my-3 block text-gray-300 hover:text-gray-100 text-sm font-medium duration-700">리크봇</a>
+<a href="#" class="my-3 block text-gray-300 hover:text-gray-100 text-sm font-medium duration-700">모임챗</a>
+<a href="#" class="my-3 block text-gray-300 hover:text-gray-100 text-sm font-medium duration-700">Unlink Developers</a>
+<a href="#" class="my-3 block text-gray-300 hover:text-gray-100 text-sm font-medium duration-700">광고</a>
 
-        <!-- Col-3 -->
-        <div class="p-5 w-1/2 sm:w-4/12 md:w-3/12">
-            <!-- Col Title -->
-            <div class="text-xs uppercase text-gray-400 font-medium mb-6">웹 서비스</div>
+</div>
 
-            <!-- Links -->
-            <a href="#" class="my-3 block text-gray-300 hover:text-gray-100 text-sm font-medium duration-700">the switch WARP</a>
-            <a href="#" class="my-3 block text-gray-300 hover:text-gray-100 text-sm font-medium duration-700">the switch DNS</a>
-            
-            
-            
-            
-            
-        </div>
+<div class="p-5 w-1/2 sm:w-4/12 md:w-3/12">
 
-        <!-- Col-3 -->
-        <div class="p-5 w-1/2 sm:w-4/12 md:w-3/12">
-            <!-- Col Title -->
-            <div class="text-xs uppercase text-gray-400 font-medium mb-6">
-                Community
-            </div>
+<div class="text-xs uppercase text-gray-400 font-medium mb-6">개인정보 센터</div>
 
-            <!-- Links -->
-            <a href="#" class="my-3 block text-gray-300 hover:text-gray-100 text-sm font-medium duration-700">
-                GitHub
-            </a>
-            <a href="#" class="my-3 block text-gray-300 hover:text-gray-100 text-sm font-medium duration-700">
-                Discord
-            </a>
-            <a href="#" class="my-3 block text-gray-300 hover:text-gray-100 text-sm font-medium duration-700">
-                Twitter
-            </a>
-            <a href="#" class="my-3 block text-gray-300 hover:text-gray-100 text-sm font-medium duration-700">
-                YouTube
-            </a>
-        </div>
-    </div>
+<a href="#" class="my-3 block text-gray-300 hover:text-gray-100 text-sm font-medium duration-700">개인정보 센터</a>
+<a href="#" class="my-3 block text-gray-300 hover:text-gray-100 text-sm font-medium duration-700">이용 약관</a>
+<a href="#" class="my-3 block text-gray-300 hover:text-gray-100 text-sm font-medium duration-700">개인정보처리방침</a>
+</div>
 
-    <!-- Copyright Bar -->
-    <div class="pt-2">
-        <div class="flex pb-5 px-3 m-auto pt-5 
+<div class="p-5 w-1/2 sm:w-4/12 md:w-3/12">
+
+<div class="text-xs uppercase text-gray-400 font-medium mb-6">웹 서비스</div>
+
+<a href="#" class="my-3 block text-gray-300 hover:text-gray-100 text-sm font-medium duration-700">the switch WARP</a>
+<a href="#" class="my-3 block text-gray-300 hover:text-gray-100 text-sm font-medium duration-700">the switch DNS</a>
+</div>
+
+
+</div>
+
+<div class="pt-2">
+<div class="flex pb-5 px-3 m-auto pt-5 
             border-t border-gray-500 text-gray-400 text-sm 
             flex-col md:flex-row max-w-6xl">
-            <div class="mt-2">
-                © 페이퍼코어 2018~2021 모든 권리 보유. <br><small>페이퍼코어로 발생한 상거래의 책임은 구매자에게 있습니다. 페이퍼코어는 사업자가 아닙니다.</small></div>
+<div class="mt-2">
+© 페이퍼코어 2018~2021 모든 권리 보유. <br><small>페이퍼코어로 발생한 상거래의 책임은 구매자에게 있습니다. 페이퍼코어는 사업자가 아닙니다.</small></div>
 
-            <!-- Required Unicons (if you want) -->
-            <div class="md:flex-auto md:flex-row-reverse mt-2 flex-row flex">
-                
-                
-                
-                
-                
-            </div>
-        </div>
-    </div>
+<div class="md:flex-auto md:flex-row-reverse mt-2 flex-row flex">
+</div>
+</div>
+</div>
 </footer>
 <script async defer src="https://scripts.simpleanalyticscdn.com/latest.js"></script>
 <noscript><img src="https://queue.simpleanalyticscdn.com/noscript.gif" alt=""/></noscript>
