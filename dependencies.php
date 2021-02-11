9c9e67ad74e0ab563c183100b7c1104ff61da21b
Leek <redev@redevs.ga>
2021-02-11 23:27:40 +0900

    Update dependencies.php

--- dependencies.php
+++ dependencies.php
@@ -1,21 +1,11 @@
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
-<link rel="apple-touch-icon" sizes="57x57" href="https://withun.link/identity/apple-icon-57x57.png">
-<link rel="apple-touch-icon" sizes="60x60" href="https://withun.link/identity/apple-icon-60x60.png">
-<link rel="apple-touch-icon" sizes="72x72" href="https://withun.link/identity/apple-icon-72x72.png">
-<link rel="apple-touch-icon" sizes="76x76" href="https://withun.link/identity/apple-icon-76x76.png">
-<link rel="apple-touch-icon" sizes="114x114" href="https://www.withun.link/identity/apple-icon-114x114.png">
-<link rel="apple-touch-icon" sizes="120x120" href="https://www.withun.link/identity/apple-icon-120x120.png">
-<link rel="apple-touch-icon" sizes="144x144" href="https://www.withun.link/identity/apple-icon-144x144.png">
-<link rel="apple-touch-icon" sizes="152x152" href="https://www.withun.link/identity/apple-icon-152x152.png">
-<link rel="apple-touch-icon" sizes="180x180" href="https://www.withun.link/identity/apple-icon-180x180.png">
-<link rel="icon" type="image/png" sizes="192x192"  href="https://www.withun.link/identity/android-icon-192x192.png">
+<link rel="apple-touch-icon" sizes="180.180" href="https://withun.link/identity/apple-touch-icon.png">
+<link rel="icon" type="image/png" sizes="192x192"  href="https://www.withun.link/identity/android-chrome-192x192.png">
 <link rel="icon" type="image/png" sizes="32x32" href="https://www.withun.link/identity/favicon-32x32.png">
-<link rel="icon" type="image/png" sizes="96x96" href="https://www.withun.link/identity/favicon-96x96.png">
 <link rel="icon" type="image/png" sizes="16x16" href="https://www.withun.link/identity/favicon-16x16.png">
 <link rel="manifest" href="https://withun.link/identity/manifest.json">
 <meta name="msapplication-TileColor" content="#1D4ED8">
 <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/@iconscout/unicons@3.0.6/css/line.css">
-<meta name="msapplication-TileImage" content="identity/ms-icon-144x144.png">
 <meta name="theme-color" content="#1D4ED8">
 <title>PaperCore | 메시징의 변화를, 삶의 변화를.</title>
 <meta name="title" content="PaperCore | 메시징의 변화를, 삶의 변화를.">
