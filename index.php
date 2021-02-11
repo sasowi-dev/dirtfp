02d9edfbbca5be6ecee43d5e86a7011de0ff95e2
Leek <redev@redevs.ga>
2021-02-11 23:06:58 +0900

    Update index.php

--- index.php
+++ index.php
@@ -6,8 +6,13 @@
   <body>
 <?php include 'evemt.php';?>
 <?php include 'header.php';?>
-설에는마음을담으세요정관장<br>
-    테스트 ㅅㄱ
+<div class="lg:2/6 xl:w-2/4 mt-20 lg:mt-40 lg:ml-16 text-left">
+            <div class="text-6xl font-semibold text-gray-900 leading-none">Bring all your work together</div>
+            <div class="mt-6 text-xl font-light text-true-gray-500 antialiased">A better experience for yout attendees and less stress yout team.</div>
+            <button class="mt-6 px-8 py-4 rounded-full font-normal tracking-wide bg-gradient-to-b from-blue-600 to-blue-700 text-white outline-none focus:outline-none hover:shadow-lg hover:from-blue-700 transition duration-200 ease-in-out">
+              Download for Free
+            </button>
+          </div>
 <?php include 'footer.php';?>
 </body>
 </html>
