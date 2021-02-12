1fcc80c5fcc71b8f93d07e75cc4a10ae519a56cb
PaperCore <redev@redevs.ga>
2021-02-12 14:59:35 +0900

    Update footer.php

--- footer.php
+++ footer.php
@@ -5,10 +5,10 @@
 
 <div class="text-xs uppercase text-gray-400 font-medium mb-6">페이퍼코어 패밀리</div>
 
-<a href="#" class="my-3 block text-gray-300 hover:text-gray-100 text-sm font-medium duration-700">리크봇</a>
-<a href="#" class="my-3 block text-gray-300 hover:text-gray-100 text-sm font-medium duration-700">모임챗</a>
-<a href="#" class="my-3 block text-gray-300 hover:text-gray-100 text-sm font-medium duration-700">Unlink Developers</a>
-<a href="#" class="my-3 block text-gray-300 hover:text-gray-100 text-sm font-medium duration-700">광고</a>
+<a href="https://withun.link/leek" class="my-3 block text-gray-300 hover:text-gray-100 text-sm font-medium duration-700">리크봇</a>
+<a href="https://withun.link/chat" class="my-3 block text-gray-300 hover:text-gray-100 text-sm font-medium duration-700">모임챗</a>
+<a href="https://withun.link/dev" class="my-3 block text-gray-300 hover:text-gray-100 text-sm font-medium duration-700">PaperCore Developers</a>
+<a href="https://withun.link/ad" class="my-3 block text-gray-300 hover:text-gray-100 text-sm font-medium duration-700">광고</a>
 
 </div>
 
@@ -16,17 +16,18 @@
 
 <div class="text-xs uppercase text-gray-400 font-medium mb-6">개인정보 센터</div>
 
-<a href="#" class="my-3 block text-gray-300 hover:text-gray-100 text-sm font-medium duration-700">개인정보 센터</a>
-<a href="#" class="my-3 block text-gray-300 hover:text-gray-100 text-sm font-medium duration-700">이용 약관</a>
-<a href="#" class="my-3 block text-gray-300 hover:text-gray-100 text-sm font-medium duration-700">개인정보처리방침</a>
+<a href="https://withun.link/privacy/" class="my-3 block text-gray-300 hover:text-gray-100 text-sm font-medium duration-700">개인정보 센터</a>
+<a href="https://withun.link/privacy/tos.html" class="my-3 block text-gray-300 hover:text-gray-100 text-sm font-medium duration-700">이용 약관</a>
+<a href="https://withun.link/privacy/eula.html" class="my-3 block text-gray-300 hover:text-gray-100 text-sm font-medium duration-700">개인정보처리방침</a>
+<a href="https://withun.link/privacy/disclaimer.html" class="my-3 block text-gray-300 hover:text-gray-100 text-sm font-medium duration-700">면책 조항</a>
 </div>
 
 <div class="p-5 w-1/2 sm:w-4/12 md:w-3/12">
 
 <div class="text-xs uppercase text-gray-400 font-medium mb-6">웹 서비스</div>
 
-<a href="#" class="my-3 block text-gray-300 hover:text-gray-100 text-sm font-medium duration-700">the switch WARP</a>
-<a href="#" class="my-3 block text-gray-300 hover:text-gray-100 text-sm font-medium duration-700">the switch DNS</a>
+<a href="https://withun.link/index-parentblock.php" class="my-3 block text-gray-300 hover:text-gray-100 text-sm font-medium duration-700">the switch WARP</a>
+<a href="https://withun.link/index-parentblock.php" class="my-3 block text-gray-300 hover:text-gray-100 text-sm font-medium duration-700">the switch DNS</a>
 </div>
 
 
