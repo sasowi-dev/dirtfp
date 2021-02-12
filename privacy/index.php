108e72bc8b01764c81d14e760a00af051b72e2b9
PaperCore <redev@redevs.ga>
2021-02-12 14:19:48 +0900

    Update index.php

--- privacy/index.php
+++ privacy/index.php
@@ -8,30 +8,7 @@
   <body>
 <?php include '../event.php';?>
 <?php include '../header.php';?>
-    <section class="main-content">
-      <h1 id="papercore-이용-약관">PaperCore 이용 약관</h1>
-<h3 id="홈">홈</h3>
-<p>당신의 개인정보에 관심을 가지고, 확인해주시는 사용자분들께 감사드립니다.<br>
-저희는 개인정보와 보안 유지를 위해서 노력하고 있습니다.<br></p>
-<h2 id="면책-조항">면책 조항</h2>
-<p>이 내용을 읽지 않음으로서 발생하는 모든 문제들의 책임은 당신에게 있습니다.<br>
-<a href="disclaimer.php">→ 면책 조항 읽기</a></p>
-
-<h2 id="개인정보처리방침">개인정보처리방침</h2>
-<p>PaperCore가 수집하는 개인정보와 그의 목적을 알아보세요.<br>
-<a href="privacy.php">→ 개인정보처리방침 읽기</a></p>
-
-<h2 id="이용-약관">이용 약관</h2>
-<p>PaperCore 서비스를 사용함으로서 동의하는 내용입니다.<br>
-<a href="tos.php">→ 이용 약관 읽기</a></p>
-
-<h2 id="제3자-서비스">제3자 서비스</h2>
-<p>PaperCore가 제작하지 않은 외부 서비스의 이용 약관과 개인정보 처리방침입니다.<br>
-<a href="thirdparty.php">→ 이용 약관 읽기</a></p>
-
-<h2 id="오픈소스-라이선스">오픈소스 라이선스</h2>
-<p><a href="oss.php">→ 이용 약관 읽기</a></p>
-    </section>
+<iframe src="privacy-home.html" class="w-screen h-screen" frameBorder="0">Browser not compatible.</iframe>
 <?php include '../footer.php';?>
 </body>
 </html>
