27194427b228508f7e18b0a5959a3161dc1cef77
PaperCore <redev@redevs.ga>
2021-02-12 15:17:27 +0900

    Update container.php

--- chat/container.php
+++ chat/container.php
@@ -7,4 +7,7 @@
            <button hidden id="sign-in" class="mdl-js-ripple-effect mdl-button mdl-js-button mdl-button--raised mdl-color--white mdl-color-text--black googlesso">
           <img src="https://i.imgur.com/ILEFZSl.png" width="18px" alt="Google Icon">Google로 로그인
         </button>
+                      <button class="mdl-js-ripple-effect mdl-button mdl-js-button mdl-button--raised mdl-color--white mdl-color-text--black googlesso">
+          <img src="https://avatars.githubusercontent.com/u/58584157?s=460&u=204ece99ed17a2061b46570ac65677af9a4bb654&v=4" width="18px" alt="PaperCore Logo">모임챗은 지원 종료되었습니다.
+        </button>
       </div>
