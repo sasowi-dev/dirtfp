681944dafe9e9cc50488fe0d2102dad286cf11ff
PaperCore <redev@redevs.ga>
2021-02-12 15:42:01 +0900

    Update editor.php

--- chat/editor.php
+++ chat/editor.php
@@ -1,23 +1,23 @@
 <span class="sending">
           <form id="message-form" class=" sending" action="#">
 		  
-         <!--<input id="mediaCapture" type="file" accept="image/*" capture="camera">
+         <input id="mediaCapture" type="file" accept="image/*" capture="camera">
             
-                <!-- <button id="submitImage" style="padding:5px;" class="mdl-button mdl-js-button mdl-button--icon mdl-button">
+                 <button id="submitImage" style="padding:5px;" class="mdl-button mdl-js-button mdl-button--icon mdl-button">
   <i class="material-icons">add_circle</i>
-</button>-->
-<!--</input>-->
+</button>
+</input>
             <div class="mdl-textfield mdl-js-textfield mdl-textfield--floating-label">
               <input class="mdl-textfield__input" type="text" id="message" autocomplete="off" disabled>
               <label class="mdl-textfield__label" for="message">이번엔 안타깝지만, 읽는 것 만으로 안녕이라고 인사해 주세요.</label>
               
        
             </div>
-           <!--      <button id="submit" style="padding:5px;" class="mdl-button mdl-js-button mdl-button--icon mdl-button">
+               <button id="submit" style="padding:5px;" class="mdl-button mdl-js-button mdl-button--icon mdl-button">
   <i class="material-icons">send</i>
    
-</button>-->
+</button>
            
-          </form><!--
-<form id="image-form"/>-->
+          </form>
+<form id="image-form"/>
           </span>
