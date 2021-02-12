e5d29a31bfa73ff9e5a4915fa6de21f6346fc256
PaperCore <redev@redevs.ga>
2021-02-12 15:05:38 +0900

    Update index.php

--- index.php
+++ index.php
@@ -37,7 +37,9 @@
                         </select>
                     </div>
                 </div>
-                <div class="mt-6">
+                <!--
+형태
+<div class="mt-6">
                     <div class="max-w-4xl px-10 py-6 bg-white rounded-lg shadow-md">
                         <div class="flex justify-between items-center"><span class="font-light text-gray-600"></span><a href="#" class="px-2 py-1 bg-gray-600 text-gray-100 font-bold rounded hover:bg-gray-500">Laravel</a>
                         </div>
@@ -53,80 +55,38 @@
                                 </a></div>
                         </div>
                     </div>
-                </div>
-                <div class="mt-6">
-                    <div class="max-w-4xl px-10 py-6 bg-white rounded-lg shadow-md">
-                        <div class="flex justify-between items-center"><span class="font-light text-gray-600">mar 4,
-                                2019</span><a href="#" class="px-2 py-1 bg-gray-600 text-gray-100 font-bold rounded hover:bg-gray-500">Design</a>
-                        </div>
-                        <div class="mt-2"><a href="#" class="text-2xl text-gray-700 font-bold hover:underline">Accessibility tools for
-                                designers and developers</a>
-                            <p class="mt-2 text-gray-600">Lorem ipsum dolor sit, amet consectetur adipisicing elit.
-                                Tempora expedita dicta totam aspernatur doloremque. Excepturi iste iusto eos enim
-                                reprehenderit nisi, accusamus delectus nihil quis facere in modi ratione libero!</p>
-                        </div>
-                        <div class="flex justify-between items-center mt-4"><a href="#" class="text-blue-500 hover:underline">Read more</a>
-                            <div><a href="#" class="flex items-center"><img src="https://images.unsplash.com/photo-1464863979621-258859e62245?ixlib=rb-1.2.1&amp;ixid=eyJhcHBfaWQiOjEyMDd9&amp;auto=format&amp;fit=crop&amp;w=333&amp;q=80" alt="avatar" class="mx-4 w-10 h-10 object-cover rounded-full hidden sm:block">
-                                    <h1 class="text-gray-700 font-bold hover:underline">Jane Doe</h1>
-                                </a></div>
-                        </div>
-                    </div>
-                </div>
-                <div class="mt-6">
-                    <div class="max-w-4xl px-10 py-6 bg-white rounded-lg shadow-md">
-                        <div class="flex justify-between items-center"><span class="font-light text-gray-600">Feb 14,
-                                2019</span><a href="#" class="px-2 py-1 bg-gray-600 text-gray-100 font-bold rounded hover:bg-gray-500">PHP</a>
-                        </div>
-                        <div class="mt-2"><a href="#" class="text-2xl text-gray-700 font-bold hover:underline">PHP:
-                                Array to Map</a>
-                            <p class="mt-2 text-gray-600">Lorem ipsum dolor sit, amet consectetur adipisicing elit.
-                                Tempora expedita dicta totam aspernatur doloremque. Excepturi iste iusto eos enim
-                                reprehenderit nisi, accusamus delectus nihil quis facere in modi ratione libero!</p>
-                        </div>
-                        <div class="flex justify-between items-center mt-4"><a href="#" class="text-blue-500 hover:underline">Read more</a>
-                            <div><a href="#" class="flex items-center"><img src="https://images.unsplash.com/photo-1531251445707-1f000e1e87d0?ixlib=rb-1.2.1&amp;ixid=eyJhcHBfaWQiOjEyMDd9&amp;auto=format&amp;fit=crop&amp;w=281&amp;q=80" alt="avatar" class="mx-4 w-10 h-10 object-cover rounded-full hidden sm:block">
-                                    <h1 class="text-gray-700 font-bold hover:underline">Lisa Way</h1>
-                                </a></div>
-                        </div>
-                    </div>
-                </div>
-                <div class="mt-6">
-                    <div class="max-w-4xl px-10 py-6 bg-white rounded-lg shadow-md">
-                        <div class="flex justify-between items-center"><span class="font-light text-gray-600">Dec 23,
-                                2018</span><a href="#" class="px-2 py-1 bg-gray-600 text-gray-100 font-bold rounded hover:bg-gray-500">Django</a>
-                        </div>
-                        <div class="mt-2"><a href="#" class="text-2xl text-gray-700 font-bold hover:underline">Django
-                                Dashboard - Learn by Coding</a>
-                            <p class="mt-2 text-gray-600">Lorem ipsum dolor sit, amet consectetur adipisicing elit.
-                                Tempora expedita dicta totam aspernatur doloremque. Excepturi iste iusto eos enim
-                                reprehenderit nisi, accusamus delectus nihil quis facere in modi ratione libero!</p>
-                        </div>
-                        <div class="flex justify-between items-center mt-4"><a href="#" class="text-blue-500 hover:underline">Read more</a>
-                            <div><a href="#" class="flex items-center"><img src="https://images.unsplash.com/photo-1500757810556-5d600d9b737d?ixlib=rb-1.2.1&amp;ixid=eyJhcHBfaWQiOjEyMDd9&amp;auto=format&amp;fit=crop&amp;w=735&amp;q=80" alt="avatar" class="mx-4 w-10 h-10 object-cover rounded-full hidden sm:block">
-                                    <h1 class="text-gray-700 font-bold hover:underline">Steve Matt</h1>
-                                </a></div>
-                        </div>
-                    </div>
-                </div>
-                <div class="mt-6">
-                    <div class="max-w-4xl px-10 py-6 bg-white rounded-lg shadow-md">
-                        <div class="flex justify-between items-center"><span class="font-light text-gray-600">Mar 10,
-                                2018</span><a href="#" class="px-2 py-1 bg-gray-600 text-gray-100 font-bold rounded hover:bg-gray-500">Testing</a>
-                        </div>
-                        <div class="mt-2"><a href="#" class="text-2xl text-gray-700 font-bold hover:underline">TDD
-                                Frist</a>
-                            <p class="mt-2 text-gray-600">Lorem ipsum dolor sit, amet consectetur adipisicing elit.
-                                Tempora expedita dicta totam aspernatur doloremque. Excepturi iste iusto eos enim
-                                reprehenderit nisi, accusamus delectus nihil quis facere in modi ratione libero!</p>
-                        </div>
-                        <div class="flex justify-between items-center mt-4"><a href="#" class="text-blue-500 hover:underline">Read more</a>
-                            <div><a href="#" class="flex items-center"><img src="https://images.unsplash.com/photo-1502980426475-b83966705988?ixlib=rb-1.2.1&amp;ixid=eyJhcHBfaWQiOjEyMDd9&amp;auto=format&amp;fit=crop&amp;w=373&amp;q=80" alt="avatar" class="mx-4 w-10 h-10 object-cover rounded-full hidden sm:block">
-                                    <h1 class="text-gray-700 font-bold hover:underline">Khatab Wedaa</h1>
-                                </a></div>
-                        </div>
-                    </div>
-                </div>
-                <div class="mt-8">
+                </div>-->
+<?php
+  try {
+  $con= new PDO('mysql:host=localhost:3306;dbname=blogfake', "root", "leegyubin!Lee06");
+  $con->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+  $query = "SELECT * FROM skills";
+  //first pass just gets the column names
+  print '<table class="min-w-full table-auto">';
+  $result = $con->query($query);
+  //return only the first row (we only need field names)
+  $row = $result->fetch(PDO::FETCH_ASSOC);
+  print " <tr>";
+  foreach ($row as $field => $value){
+   print ' <th></th>';
+  } // end foreach
+  print " </tr>";
+  //second query gets the data
+  $data = $con->query($query);
+  $data->setFetchMode(PDO::FETCH_ASSOC);
+  foreach($data as $row){
+   print " <tr>";
+   foreach ($row as $name=>$value){
+   print " <td>$value</td>";
+   } // end field loop
+   print " </tr>";
+  } // end record loop
+  print "</table>";
+  } catch(PDOException $e) {
+   echo 'ERROR: ' . $e->getMessage();
+  } // end try
+ ?>
+              <!-- <div class="mt-8">
                     <div class="flex">
                         <a href="#" class="mx-1 px-3 py-2 bg-white text-gray-500 font-medium rounded-md cursor-not-allowed">
                             previous
@@ -148,7 +108,7 @@
                             Next
                         </a>
                     </div>
-                </div>
+                </div>-->
             </div>
             <div class="-mx-8 w-4/12 hidden lg:block">
                 
