d50a3d447190a5d2cb2d717061076a473badc289
Leek <redev@redevs.ga>
2021-02-12 13:22:04 +0900

    Create index.php

---
+++ privacy/index.php
@@ -0,0 +1,21 @@
+  
+<!DOCTYPE HTML>
+<html lang="ko">
+<head>
+<meta charset="utf-8">
+<?php include '../dependencies.php';?>
+  </head>
+  <body>
+<?php include '../event.php';?>
+<?php include '../header.php';?>
+<div class="container mx-auto px-6 sm:px-12 flex flex-col-reverse sm:flex-row items-center">
+        <div class="sm:w-2/5 flex flex-col items-start mt-8 sm:mt-0">
+            <h1 class="text-4xl lg:text-6xl leading-none mb-4"><strong class="font-black">개인정보 보호가</strong>우선입니다.</h1>
+            <p class="lg:text-lg mb-4 sm:mb-12">다수의 웹사이트가 그렇지는 않지만, 당신의 개인정보가 기능의 우선점이고 당신이 원하는 대로 켜고 끌 수 있는 개인정보가 필요합니다.</p>
+            <a href="#" class="font-semibold text-lg bg-indigo-500 hover:bg-indigo-400 text-white py-3 px-10 rounded-full">빠른 EULA 읽기</a>
+        </div>
+        
+    </div>
+<?php include '../footer.php';?>
+</body>
+</html>
