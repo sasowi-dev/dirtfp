1fcd2d87623953bf2861b489c06d22ab30d99500
Leek <redev@redevs.ga>
2021-02-12 11:49:53 +0900

    Update index.php

--- leek/index.php
+++ leek/index.php
@@ -86,43 +86,8 @@ function sliderButton1(){
 
     <body>
 	<?php include '../event.php'; ?>
-<script src="https://www.gstatic.com/dialogflow-console/fast/messenger-cx/bootstrap.js?v=1"></script>
-<df-messenger
-  df-cx="true"
-  location="asia-northeast1"
-  chat-title="LeekBot"
-  agent-id="d403fff2-b6e1-47ea-a78a-476f5a1aae68"
-  language-code="ko"
-></df-messenger>
- <!-- This example requires Tailwind CSS v2.0+ -->
-<div class="bg-indigo-600">
-  <div class="max-w-7xl mx-auto py-3 px-3 sm:px-6 lg:px-8">
-    <div class="flex items-center justify-between flex-wrap">
-      <div class="w-0 flex-1 flex items-center">
-        <span class="flex p-2 rounded-lg bg-indigo-800">
-          <!-- Heroicon name: speakerphone -->
-          <svg class="h-6 w-6 text-white" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true">
-            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M11 5.882V19.24a1.76 1.76 0 01-3.417.592l-2.147-6.15M18 13a3 3 0 100-6M5.436 13.683A4.001 4.001 0 017 6h1.832c4.1 0 7.625-1.234 9.168-3v14c-1.543-1.766-5.067-3-9.168-3H7a3.988 3.988 0 01-1.564-.317z" />
-          </svg>
-        </span>
-        <p class="ml-3 font-medium text-white truncate">
-          <span class="md:hidden">
-            팀샐 팬방 VS 리크봇 로그를 문의하여 요청
-          </span>
-          <span class="hidden md:inline">
-            팀샐 팬방에서의 리크봇 밴에 대해 로그를 투명하게 공개합니다. 요구시 Google Drive 링크를 전송합니다
-          </span>
-        </p>
-      </div>
+<?php include '../header.php'; ?>
 
-    </div>
-  </div>
-</div>
-        <!-- 제목부분 -->
-            <header  class="shadow bg-white index9 flex items-center justify-between flex-wrap p-6 sticky top-0"><meta http-equiv="Content-Type" content="text/html; charset=utf-8">
-  <div class="flex items-center flex-shrink-0 text-black mr-6">
-      <!--웹페이지 기본 정보 -->
-<a href="index.php"><img src="leekbot-logo.png" class="logobox" alt="Unlink"></a></div>
 <style>
 html{
 	  scroll-behavior: smooth;
@@ -160,7 +125,6 @@ html{
 }
 </style>
 
-</header>
  <div class="sliderAx h-auto">
       <div id="slider-1" class="container mx-auto">
         <div class="bg-cover bg-center  h-auto text-white py-24 px-10 object-fill" style="background-image: url(bg-1.png)">
@@ -384,6 +348,7 @@ html{
 <br><br>
 그리고 방장의 동의 없이 리크봇을 초대한 사용자에게는 적절한 제재를 가하겠습니다.</p>
 </div>-->
+		   <?php include '../footer.php'; ?>
 	  </body>
 
 </html>
