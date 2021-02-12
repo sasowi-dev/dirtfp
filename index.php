76350d7088c5c3c0af4ad1e9dea85b8f5df13e56
Leek <redev@redevs.ga>
2021-02-12 11:21:06 +0900

    Update index.php

--- index.php
+++ index.php
@@ -23,72 +23,71 @@
                 </svg>
             </bottom>
           </div>
-    <img src="identity/StartOfPaperCore.svg" alt="페이퍼코어의 시작" class="h-14">
-    <div class="relative w-1/2 m-8">
-            <div class="border-r-2 border-gray-200 border-dotted absolute h-full top-0 z-0" style="left: 7px"></div>
-            <ul class="list-none m-0 p-0">
-                <li class="mb-2">
-                    <div class="flex items-center mb-1">
-                        <div class="bg-indigo-600 rounded-full h-4 w-4 border-gray-200 border-2 z-10">
-                        </div>
-                        <div class="flex-1 ml-4 font-medium">Oct 2020 - Learn Electron JS</div>
-                    </div>
-                    <div class="ml-12">
-                        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dignissimos tenetur, suscipit atque et
-                        nulla dolores unde maiores doloribus nemo possimus commodi totam consectetur temporibus odit
-                        velit illo repellendus provident vitae.
-                    </div>
-                </li>
-                <li class="mb-2">
-                    <div class="flex items-center mb-1">
-                        <div class="bg-indigo-600 rounded-full h-4 w-4 border-gray-200 border-2 z-10">
-                        </div>
-                        <div class="flex-1 ml-4 font-medium">May 2019 - Learn Golang</div>
-                    </div>
-                    <div class="ml-12">
-                        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dignissimos tenetur, suscipit atque et
-                        nulla dolores unde maiores doloribus nemo possimus commodi totam consectetur temporibus odit
-                        velit illo repellendus provident vitae.
-                    </div>
-                </li>
-                <li class="mb-2">
-                    <div class="flex items-center mb-1">
-                        <div class="bg-indigo-600 rounded-full h-4 w-4 border-gray-200 border-2 z-10">
-                        </div>
-                        <div class="flex-1 ml-4 font-medium">April 2019 - Learn Photography</div>
-                    </div>
-                    <div class="ml-12">
-                        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dignissimos tenetur, suscipit atque et
-                        nulla dolores unde maiores doloribus nemo possimus commodi totam consectetur temporibus odit
-                        velit illo repellendus provident vitae.
-                    </div>
-                </li>
-                <li class="mb-2">
-                    <div class="flex items-center mb-1">
-                        <div class="bg-indigo-600 rounded-full h-4 w-4 border-gray-200 border-2 z-10">
-                        </div>
-                        <div class="flex-1 ml-4 font-medium">Feb 2018 - Learn Laravel</div>
-                    </div>
-                    <div class="ml-12">
-                        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dignissimos tenetur, suscipit atque et
-                        nulla dolores unde maiores doloribus nemo possimus commodi totam consectetur temporibus odit
-                        velit illo repellendus provident vitae.
-                    </div>
-                </li>
-                <li class="mb-2">
-                    <div class="flex items-center mb-1">
-                        <div class="bg-indigo-600 rounded-full h-4 w-4 border-gray-200 border-2 z-10">
-                        </div>
-                        <div class="flex-1 ml-4 font-medium">May 2017 - Learn PHP</div>
-                    </div>
-                    <div class="ml-12">
-                        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dignissimos tenetur, suscipit atque et
-                        nulla dolores unde maiores doloribus nemo possimus commodi totam consectetur temporibus odit
-                        velit illo repellendus provident vitae.
-                    </div>
-                </li>
-            </ul>
-        </div>
+<div class="mt-12 lg:mt-32 lg:ml-20 text-left relative w-2/3 m-8">
+<div class="border-r-2 border-gray-200 border-dotted absolute h-full top-0 z-0" style="left: 7px"></div>
+<ul class="list-none m-0 p-0">
+<li class="mb-2">
+<div class="flex items-center mb-1">
+<div class="bg-indigo-600 rounded-full h-4 w-4 border-gray-200 border-2 z-10">
+</div>
+<div class="flex-1 ml-4 font-medium">Oct 2020 - Learn Electron JS</div>
+</div>
+<div class="ml-12">
+Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dignissimos tenetur, suscipit atque et
+nulla dolores unde maiores doloribus nemo possimus commodi totam consectetur temporibus odit
+velit illo repellendus provident vitae.
+</div>
+</li>
+<li class="mb-2">
+<div class="flex items-center mb-1">
+<div class="bg-indigo-600 rounded-full h-4 w-4 border-gray-200 border-2 z-10">
+</div>
+<div class="flex-1 ml-4 font-medium">May 2019 - Learn Golang</div>
+</div>
+<div class="ml-12">
+Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dignissimos tenetur, suscipit atque et
+nulla dolores unde maiores doloribus nemo possimus commodi totam consectetur temporibus odit
+velit illo repellendus provident vitae.
+</div>
+</li>
+<li class="mb-2">
+<div class="flex items-center mb-1">
+<div class="bg-indigo-600 rounded-full h-4 w-4 border-gray-200 border-2 z-10">
+</div>
+<div class="flex-1 ml-4 font-medium">April 2019 - Learn Photography</div>
+</div>
+<div class="ml-12">
+Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dignissimos tenetur, suscipit atque et
+nulla dolores unde maiores doloribus nemo possimus commodi totam consectetur temporibus odit
+velit illo repellendus provident vitae.
+</div>
+</li>
+<li class="mb-2">
+<div class="flex items-center mb-1">
+<div class="bg-indigo-600 rounded-full h-4 w-4 border-gray-200 border-2 z-10">
+</div>
+<div class="flex-1 ml-4 font-medium">Feb 2018 - Learn Laravel</div>
+</div>
+<div class="ml-12">
+Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dignissimos tenetur, suscipit atque et
+nulla dolores unde maiores doloribus nemo possimus commodi totam consectetur temporibus odit
+velit illo repellendus provident vitae.
+</div>
+</li>
+<li class="mb-2">
+<div class="flex items-center mb-1">
+<div class="bg-indigo-600 rounded-full h-4 w-4 border-gray-200 border-2 z-10">
+</div>
+<div class="flex-1 ml-4 font-medium">May 2017 - Learn PHP</div>
+</div>
+<div class="ml-12">
+Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dignissimos tenetur, suscipit atque et
+nulla dolores unde maiores doloribus nemo possimus commodi totam consectetur temporibus odit
+velit illo repellendus provident vitae.
+</div>
+</li>
+</ul>
+</div>
 <?php include 'footer.php';?>
 </body>
 </html>
