02ef5c49be85778e05738dfe3cb840ac123129d3
PaperCore <redev@redevs.ga>
2021-02-24 13:33:49 +0900

    Update header.php

--- header.php
+++ header.php
@@ -46,7 +46,9 @@
     position: relative;
     top: -1px;
   }
-
+.socialicons{
+	height:1rem;
+	}
   </style>
 
 <nav class="relative bg-white border-b-2 border-gray-300 text-gray-900">
@@ -59,7 +61,7 @@
       <!--Regular Link-->
 	    
       <li class="hover:bg-blue-800 hover:text-white">
-        <a href="#" class="relative block py-6 px-2 lg:p-6 text-sm lg:text-base font-bold">GitHub</a>
+        <a href="#" class="relative block py-6 px-2 lg:p-6 text-sm lg:text-base font-bold"><img src="socialicons/gh-mini.png" alt="깃허브 옥토캣" class="socialicons">GitHub</a>
       </li>
 	
 		  
