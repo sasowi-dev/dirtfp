71e8b6495b0b3c7a38bc4f0a572d0265344e0d8b
PaperCore <redev@redevs.ga>
2021-02-24 13:03:12 +0900

    Update index.php

--- index.php
+++ index.php
@@ -7,23 +7,61 @@
   <body>
 <?php include 'event.php';?>
 <?php include 'header.php';?>
-<div class="lg:2/6 xl:w-2/4 mt-20 lg:mt-40 lg:ml-16 text-left">
-            <div class="text-4xl font-semibold text-gray-900 leading-none">
-<img src="identity/slogan-1.png" alt="메시징의 변화를," class="h-14">
-<img src="identity/slogan-2.png" alt="삶의 변화를," class="h-14">
-  </div>
-            <div class="mt-6 text-xl font-light text-true-gray-500 antialiased">PaperCore는 여태껏 경험하지 못한 메시징과 글자의 가능성을 2020년부터 나타냈습니다. 그리고, 그 여정은 오늘까지도 이어집니다.</div>
-            <button class="mt-6 px-8 py-4 rounded-full font-normal tracking-wide bg-indigo-700 text-white outline-none focus:outline-none hover:shadow-lg hover:from-blue-700 transition duration-200 ease-in-out">
-              자세히 알아보기
-            </button>
-          </div>
-   <div class="mt-12 lg:mt-32 lg:ml-20 text-left">
-            <bottom type="button" class="flex items-center justify-center w-12 h-12 rounded-full bg-cool-gray-100 text-gray-800 animate-bounce hover:text-gray-900 hover:bg-cool-gray-50 transition duration-300 ease-in-out cursor-pointer">
-                <svg class="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
-                  <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M19 14l-7 7m0 0l-7-7m7 7V3"></path>
-                </svg>
-            </bottom>
-          </div>
+<script src="https://code.jquery.com/jquery-2.2.4.min.js"></script>
+<style>
+.star {
+  position: absolute;
+  width: 2px;
+  height: 2px;
+  border-radius: 5px;
+}
+
+@keyframes twinkle {
+  0% {
+    transform: scale(1, 1);
+    background: rgba(255, 255, 255, 0);
+    animation-timing-function: linear;
+  }
+  40% {
+    transform: scale(0.8, 0.8);
+    background: rgba(255, 255, 255, 1);
+    animation-timing-function: ease-out;
+  }
+  80% {
+    background: rgba(255, 255, 255, 0);
+    transform: scale(1, 1);
+  }
+  100% {
+    background: rgba(255, 255, 255, 0);
+    transform: scale(1, 1);
+  }
+}
+</style>
+
+<section
+      class="homescreen m-0 flex flex-col w-screen justify-center bg-gray-800 h-screen text-gray-100 "
+    >
+
+      <h1 class="text-6xl  my-auto mx-auto  md:mx-48 ">
+<img src="identity/sg.svg" alt="메시징의 변화를, 삶의 변화를. 2018년부터 시작된 그 여정으로 오픈소스와 소프트웨어 사용자들에게 더 나은 선택을 드립">
+      </h1>
+    </section>
+<script>
+for (var i = 0; i < 100; i++) {
+  var star =
+    '<div class="star m-0" style="animation: twinkle ' +
+    (Math.random() * 5 + 5) +
+    's linear ' +
+    (Math.random() * 1 + 1) +
+    's infinite; top: ' +
+    Math.random() * $(window).height() +
+    'px; left: ' +
+    Math.random() * $(window).width() +
+    'px;"></div>';
+  $('.homescreen').append(star);
+}
+
+</script>
 
 <div class="px-6 py-8">
         <div class="flex justify-between container mx-auto">
