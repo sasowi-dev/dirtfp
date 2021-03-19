21b67d40f29e43a86c27ba94a979b842a0153709
PaperCore <redev@redevs.ga>
2021-03-19 09:19:06 +0900

    Update commands.php

--- leek/commands.php
+++ leek/commands.php
@@ -66,36 +66,6 @@
 
 		  <center>
 <p>
- <?php
-  try {
-  $con= new PDO('mysql:host=localhost:3306;dbname=leekcommands', "root", "leegyubin!Lee06");
-  $con->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-  $query = "SELECT * FROM skills";
-  //first pass just gets the column names
-  print '<table class="min-w-full table-auto">';
-  $result = $con->query($query);
-  //return only the first row (we only need field names)
-  $row = $result->fetch(PDO::FETCH_ASSOC);
-  print " <tr>";
-  foreach ($row as $field => $value){
-   print ' <th><h1 class="text-4xl lg:text-6xl leading-none mb-4">오늘은 무엇을 할까요?</h1></th>';
-  } // end foreach
-  print " </tr>";
-  //second query gets the data
-  $data = $con->query($query);
-  $data->setFetchMode(PDO::FETCH_ASSOC);
-  foreach($data as $row){
-   print " <tr>";
-   foreach ($row as $name=>$value){
-   print " <td>$value</td>";
-   } // end field loop
-   print " </tr>";
-  } // end record loop
-  print "</table>";
-  } catch(PDOException $e) {
-   echo 'ERROR: ' . $e->getMessage();
-  } // end try
- ?>
  </p>
   </center>
     </section>
