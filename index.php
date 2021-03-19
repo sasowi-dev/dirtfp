dbce4f930f0abc3e134251d8164af03c0b327bf5
PaperCore <redev@redevs.ga>
2021-03-19 09:19:21 +0900

    Update index.php

--- index.php
+++ index.php
@@ -77,36 +77,7 @@
                         </div>
                     </div>
                 </div>-->
-<?php
-  try {
-  $con= new PDO('mysql:host=localhost:3306;dbname=blogfake', "root", "leegyubin!Lee06");
-  $con->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-  $query = "SELECT * FROM skills";
-  //first pass just gets the column names
-  print '<table class="min-w-full table-auto">';
-  $result = $con->query($query);
-  //return only the first row (we only need field names)
-  $row = $result->fetch(PDO::FETCH_ASSOC);
-  print " <tr>";
-  foreach ($row as $field => $value){
-   print ' <th></th>';
-  } // end foreach
-  print " </tr>";
-  //second query gets the data
-  $data = $con->query($query);
-  $data->setFetchMode(PDO::FETCH_ASSOC);
-  foreach($data as $row){
-   print " <tr>";
-   foreach ($row as $name=>$value){
-   print " <td>$value</td>";
-   } // end field loop
-   print " </tr>";
-  } // end record loop
-  print "</table>";
-  } catch(PDOException $e) {
-   echo 'ERROR: ' . $e->getMessage();
-  } // end try
- ?>
+
               <!-- <div class="mt-8">
                     <div class="flex">
                         <a href="#" class="mx-1 px-3 py-2 bg-white text-gray-500 font-medium rounded-md cursor-not-allowed">
