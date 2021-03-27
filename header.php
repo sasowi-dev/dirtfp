e26c23d9954fb726f60a359ab6b4661018441801
PaperCore <redev@redevs.ga>
2021-03-27 18:41:01 +0900

    Update header.php

--- header.php
+++ header.php
@@ -1,61 +1,27 @@
-<style>
-  .mega-menu {
-    display: none;
-    left: 0;
-    position: absolute;
-    text-align: left;
-    width: 100%;
-  }
-  .hoverable {
-    position: static;
-  }
-  .hoverable > a:after {
-    content: "\25BC";
-    font-size: 10px;
-    padding-left: 6px;
-    position: relative;
-    top: -1px;
-  }
-  .hoverable:hover .mega-menu {
-    display: block;
-  }
-  .toggleable > label:after {
-    content: "\25BC";
-    font-size: 10px;
-    padding-left: 6px;
-    position: relative;
-    top: -1px;
-  }
-  .toggle-input {
-    display: none;
-  }
-  .toggle-input:not(checked) ~ .mega-menu {
-    display: none;
-  }
-  .toggle-input:checked ~ .mega-menu {
-    display: block;
-  }
-  .toggle-input:checked + label {
-    color: white;
-    background: #2c5282;
-  }
-  .toggle-input:checked ~ label:after {
-    content: "\25B2";
-    font-size: 10px;
-    padding-left: 6px;
-    position: relative;
-    top: -1px;
-  }
-.socialicons{
-	height:1rem;
-	}
-  </style>
+<header class="bg-white shadow-sm w-full fixed z-10  top-0 ">
+        <div class="max-auto  px-8 py-2 bg-white">
+         <div class="flex justify-between">
+		 <a href="https://flipstore.withun.link">
+            <div class="logo flex items-center space-x-4 mr-10">
+              <img class="h-8" src="https://flipstore.withun.link/identity/Group%201.svg" alt="">
 
-<nav class="relative bg-white border-b-2 border-gray-300 text-gray-900">
-  <div class="container mx-auto flex justify-between">
-	     <a href="https://flipstore.withun.link/">
-    <div class="flex items-center flex-no-shrink relative block p-4 lg:p-6 text-xl text-gray-600">
-	 <img src="https://flipstore.withun.link/identity/Group%201.svg" class="h-5" alt="FP play logo area">FP Play</div></a>
-      </div>
-    </nav>
-<div>
+                  <h1 class="text-gray-600 text-2xl">Play</h1>
+            </div>
+		 </a>
+            <div class=" menu flex justify-end  items-center  flex-1 space-x-4">
+<a href="https://withun.link">
+                  <svg  class="h-9 rounded-full px-2 py-2  text-gray-500 hover:bg-gray-200" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6a2 2 0 012-2h2a2 2 0 012 2v2a2 2 0 01-2 2H6a2 2 0 01-2-2V6zM14 6a2 2 0 012-2h2a2 2 0 012 2v2a2 2 0 01-2 2h-2a2 2 0 01-2-2V6zM4 16a2 2 0 012-2h2a2 2 0 012 2v2a2 2 0 01-2 2H6a2 2 0 01-2-2v-2zM14 16a2 2 0 012-2h2a2 2 0 012 2v2a2 2 0 01-2 2h-2a2 2 0 01-2-2v-2z" />
+                  </svg>
+</a>
+                  <img class="h-9 rounded-full border border-gray-100 shadow-sm" src="https://i.stack.imgur.com/34AD2.jpg" alt="user image" />
+            </div>
+         </div>
+        </div>
+    </header>
+
+  <style>
+    body{
+      background-color:white;
+    }
+  </style>
