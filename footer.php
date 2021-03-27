7d17908f04e619461bd13c026f1269bbc32b90b3
PaperCore <redev@redevs.ga>
2021-03-27 18:49:09 +0900

    Update footer.php

--- footer.php
+++ footer.php
@@ -6,20 +6,7 @@
 <div class="text-xs uppercase text-gray-800 font-medium mb-6">추천</div>
 
 <a href="https://www.apps4flip.com/" class="my-3 block text-gray-800 hover:text-gray-900 text-sm font-medium duration-700">Apps4Flip</a>
-<a href="https://cafe.naver.com/luckyvenusfolder/" class="my-3 block text-gray-800 hover:text-gray-900 text-sm font-medium duration-700"></a>
+<a href="https://cafe.naver.com/luckyvenusfolder/" class="my-3 block text-gray-800 hover:text-gray-900 text-sm font-medium duration-700">네이버 카페</a>
 
-</div>
-
-
-<div class="pt-2">
-<div class="flex pb-5 px-3 m-auto pt-5 
-            border-t border-gray-500 text-gray-800 text-sm 
-            flex-col md:flex-row max-w-6xl">
-<div class="mt-2">
-&copy; FPPlay</div>
-
-<div class="md:flex-auto md:flex-row-reverse mt-2 flex-row flex">
-</div>
-</div>
 </div>
 </footer>
