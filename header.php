16ae346adfe3a3e5f5c41746b37f75d084ba21f9
PaperCore <redev@redevs.ga>
2021-03-27 22:22:21 +0900

    Update header.php

--- header.php
+++ header.php
@@ -8,12 +8,14 @@
 </div>
 </a>
 <div class=" menu flex justify-end  items-center  flex-1 space-x-4">
-<a href="https://withun.link">
+<a href="https://flipstore.withun.link/about">
 <svg class="h-9 rounded-full px-2 py-2  text-gray-500 hover:bg-gray-200" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
 <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6a2 2 0 012-2h2a2 2 0 012 2v2a2 2 0 01-2 2H6a2 2 0 01-2-2V6zM14 6a2 2 0 012-2h2a2 2 0 012 2v2a2 2 0 01-2 2h-2a2 2 0 01-2-2V6zM4 16a2 2 0 012-2h2a2 2 0 012 2v2a2 2 0 01-2 2H6a2 2 0 01-2-2v-2zM14 16a2 2 0 012-2h2a2 2 0 012 2v2a2 2 0 01-2 2h-2a2 2 0 01-2-2v-2z"></path>
 </svg>
 </a>
+    <a href="https://flipstore.withun.link/account">
 <img class="h-9 rounded-full border border-gray-100 shadow-sm" src="https://i.stack.imgur.com/34AD2.jpg" alt="user image">
+    </a>
 </div>
 </div>
 </div>
