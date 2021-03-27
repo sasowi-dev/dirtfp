fbde8bd81c731d3445340d5aa0fff07ea81e26c1
PaperCore <redev@redevs.ga>
2021-03-27 19:15:18 +0900

    Update footer.php

--- footer.php
+++ footer.php
@@ -1,6 +1,7 @@
 <footer class="bg-white pt-10 sm:mt-10 pt-10">
 <div class="max-w-6xl m-auto text-gray-800 flex flex-wrap justify-left">
 
+  <div class="text-xs uppercase text-gray-800 font-xs mb-6"><strong>팁! </strong> 이 페이지는 폴더폰에 최적화되어 있습니다.</div>
 <div class="p-5 w-full sm:w-4/12 md:w-3/12">
 
 <div class="text-xs uppercase text-gray-800 font-medium mb-6">추천</div>
