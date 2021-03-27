9c25f7914d58e83d295d003bd6e6526386f2f4d1
PaperCore <redev@redevs.ga>
2021-03-27 22:37:07 +0900

    Create index.php

---
+++ devenroll/index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE HTML>
+<html lang="ko">
+<head>
+<meta charset="utf-8">
+<?php include '../dependencies.php';?>
+  </head>
+  <body>
+<?php include '../event.php';?>
+<?php include '../header.php';?>
+<div class="bg-gray-100 m-auto w-full h-64 mt-5" style="background-image:url('reviewland.svg'); background-position: center; background-repeat: no-repeat; background-size: cover;">
+<div class="flex flex-row items-end h-full w-full">
+<div class="flex flex-col w-full pb-3 pt-10 px-3 bg-gradient-to-t from-black text-gray-200">
+<h3 class="text-base font-bold leading-5 uppercase">개발자 프로그램 신청</h3>
+</div>
+</div>
+</div>
+    
+    
+    
+<?php include '../footer.php';?>
+</body>
+</html>
