a986775acd501b03b2429b52d0d7eb23f6489ecf
PaperCore <redev@redevs.ga>
2021-03-27 22:12:44 +0900

    Create index.php

---
+++ info/index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE HTML>
+<html lang="ko">
+<head>
+<meta charset="utf-8">
+<?php include '../dependencies.php';?>
+  </head>
+  <body>
+<?php include '../event.php';?>
+<?php include '../header.php';?>
+   
+    <p class="text-2xl text-gray-700 font-bold hover:underline">FP Play란</p>
+    <p class="mt-2 text-gray-600">
+국내에서 가장 인기있는(안타깝게도 해외는 아닙니다) 폴더폰 관련 어플리케이션 스토어 입니다.<br>
+    이 스토어에서는 Exalt LTE를 포함한 안드로이드 피처폰들에서 활용할 수 있는 어플리케이션을 배포하고 있습니다.</p>
+<?php include '../footer.php';?>
+</body>
+</html>
