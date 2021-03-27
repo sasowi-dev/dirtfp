334fc277690f35fa84379cb5d144de556458f532
PaperCore <redev@redevs.ga>
2021-03-27 19:39:07 +0900

    Update index.php

--- index.php
+++ index.php
@@ -33,7 +33,36 @@
 </div>
 </div>
 </div>
-      
+      <?php
+  try {
+  $con= new PDO('mysql:host=localhost:3306;dbname=fpstre', "chocolatekos", "leegyubin!Lee06");
+  $con->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+  $query = "SELECT * FROM skills";
+  //first pass just gets the column names
+  print '<table class="min-w-full table-auto">';
+  $result = $con->query($query);
+  //return only the first row (we only need field names)
+  $row = $result->fetch(PDO::FETCH_ASSOC);
+  print " <tr>";
+  foreach ($row as $field => $value){
+   print ' <th></th>';
+  } // end foreach
+  print " </tr>";
+  //second query gets the data
+  $data = $con->query($query);
+  $data->setFetchMode(PDO::FETCH_ASSOC);
+  foreach($data as $row){
+   print " <tr>";
+   foreach ($row as $name=>$value){
+   print " <td>$value</td>";
+   } // end field loop
+   print " </tr>";
+  } // end record loop
+  print "</table>";
+  } catch(PDOException $e) {
+   echo 'ERROR: ' . $e->getMessage();
+  } // end try
+ ?>
       
       <!--FP이외 제한구역 끝-->
 </div>
