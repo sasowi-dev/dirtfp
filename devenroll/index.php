4bed168aa0e8bc1ccc98e45cdf75f4efcd149993
PaperCore <redev@redevs.ga>
2021-03-27 22:41:39 +0900

    Update index.php

--- devenroll/index.php
+++ devenroll/index.php
@@ -7,7 +7,7 @@
   <body>
 <?php include '../event.php';?>
 <?php include '../header.php';?>
-<div class="bg-gray-100 m-auto w-full h-64 mt-5" style="background-image:url('reviewland.svg'); background-position: center; background-repeat: no-repeat; background-size: cover;">
+<div class="bg-gray-100 m-auto w-full h-64 mt-5" style="background-image:url('reviewland.png'); background-position: center; background-repeat: no-repeat; background-size: cover;">
 <div class="flex flex-row items-end h-full w-full">
 <div class="flex flex-col w-full pb-3 pt-10 px-3 bg-gradient-to-t from-black text-gray-200">
 <h3 class="text-base font-bold leading-5 uppercase">개발자 프로그램 신청</h3>
