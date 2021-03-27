8f21686b83efd74c4a56fc093abb30125ce66dc3
PaperCore <redev@redevs.ga>
2021-03-27 18:27:53 +0900

    Update dependencies.php

--- dependencies.php
+++ dependencies.php
@@ -1,28 +1,22 @@
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
-<link rel="apple-touch-icon" sizes="180.180" href="https://withun.link/identity/apple-touch-icon.png">
-<link rel="icon" type="image/png" sizes="192x192"  href="https://www.withun.link/identity/android-chrome-192x192.png">
-<link rel="icon" type="image/png" sizes="32x32" href="https://www.withun.link/identity/favicon-32x32.png">
-<link rel="icon" type="image/png" sizes="16x16" href="https://www.withun.link/identity/favicon-16x16.png">
+<link rel="apple-touch-icon" sizes="180.180" href="https://flipstore.withun.link/identity/apple-touch-icon.png">
+<link rel="icon" type="image/png" sizes="192x192"  href="https://flipstore.withun.link/identity/android-chrome-192x192.png">
+<link rel="icon" type="image/png" sizes="32x32" href="https://flipstore.withun.link/identity/favicon-32x32.png">
+<link rel="icon" type="image/png" sizes="16x16" href="https://flipstore.withun.link/identity/favicon-16x16.png">
 <link rel="manifest" href="https://withun.link/identity/site.webmanifest">
 <meta name="msapplication-TileColor" content="#1D4ED8">
 <meta name="theme-color" content="#1D4ED8">
-<title>PaperCore | 메시징의 변화를, 삶의 변화를.</title>
-<meta name="title" content="PaperCore | 메시징의 변화를, 삶의 변화를.">
-<meta name="description" content="Unlink는 PaperCore의 패밀리입니다.">
+<title>FP Play</title>
+<meta name="title" content="FP Play">
+<meta name="description" content="플립폰(폴더폰)을 위한 어플리케이션 스토어">
 <meta property="og:type" content="website">
-<meta property="og:url" content="https://www.withun.link/">
-<meta property="og:title" content="PaperCore | 메시징의 변화를, 삶의 변화를.">
-<meta property="og:description" content="Unlink는 PaperCore의 패밀리입니다.">
-<meta property="og:image" content="https://www.withun.link/identity/papercore_new.png">
+<meta property="og:url" content="https://flipstore.withun.link/">
+<meta property="og:title" content="FP Play">
+<meta property="og:description" content="플립폰(폴더폰)을 위한 어플리케이션 스토어">
+<meta property="og:image" content="https://flipstore.withun.link/identity/papercore_new.png">
 <meta property="twitter:card" content="summary_large_image">
-<meta property="twitter:url" content="https://www.withun.link/">
-<meta property="twitter:title" content="PaperCore | 메시징의 변화를, 삶의 변화를.">
-<meta property="twitter:description" content="Unlink는 PaperCore의 패밀리입니다.">
-<meta property="twitter:image" content="https://www.withun.link/identity/papercore_new.png">
-<!--<link href="https://www.withun.link/code/unlinkui.css" rel="stylesheet">-->
+<meta property="twitter:url" content="https://flipstore.withun.link/">
+<meta property="twitter:title" content="FP Play">
+<meta property="twitter:description" content="플립폰(폴더폰)을 위한 어플리케이션 스토어">
+<meta property="twitter:image" content="https://flipstore.withun.link/identity/papercore_new.png">
 <link href="https://cdnjs.cloudflare.com/ajax/libs/tailwindcss/2.0.3/tailwind.min.css" rel="stylesheet">
-<!--
-<script>
-var brand="PaperCore";0===location.pathname.indexOf("dependencies")?document.title=brand+" | 중요 파일! 접근 금지!!":0===location.pathname.indexOf("/store.php")?document.title=brand+" | 다운로드 스토어":0===location.pathname.indexOf("/chat")?document.title=brand+" | 모임챗":0===location.pathname.indexOf("/leek")?document.title=brand+" | 리크봇":0===location.pathname.indexOf("/chat")?document.title=brand+" | 모임챗":0===location.pathname.indexOf("/covidr")||0===location.pathname.indexOf("/hcs")?document.title=brand+" | 코로나19":"/"===location.pathname?document.title=brand+" | 홈":document.title=brand;
-</script>-->
-
