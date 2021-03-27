4b7f5e1ecb0452674efd999e26eebf6ac2f74585
PaperCore <redev@redevs.ga>
2021-03-27 18:48:11 +0900

    Update footer.php

--- footer.php
+++ footer.php
@@ -1,48 +1,25 @@
-<footer class="bg-gray-800 pt-10 sm:mt-10 pt-10">
+<footer class="bg-white pt-10 sm:mt-10 pt-10">
 <div class="max-w-6xl m-auto text-gray-800 flex flex-wrap justify-left">
 
-<div class="p-5 w-1/2 sm:w-4/12 md:w-3/12">
+<div class="p-5 w-full sm:w-4/12 md:w-3/12">
 
-<div class="text-xs uppercase text-gray-400 font-medium mb-6">페이퍼코어 패밀리</div>
+<div class="text-xs uppercase text-gray-800 font-medium mb-6">추천</div>
 
-<a href="https://withun.link/leek" class="my-3 block text-gray-300 hover:text-gray-100 text-sm font-medium duration-700">리크봇</a>
-<a href="https://withun.link/dev" class="my-3 block text-gray-300 hover:text-gray-100 text-sm font-medium duration-700">PaperCore Developers</a>
-<a href="https://withun.link/ad" class="my-3 block text-gray-300 hover:text-gray-100 text-sm font-medium duration-700">광고</a>
-
-</div>
-
-<div class="p-5 w-1/2 sm:w-4/12 md:w-3/12">
-
-<div class="text-xs uppercase text-gray-400 font-medium mb-6">개인정보 센터</div>
-
-<a href="https://withun.link/privacy/" class="my-3 block text-gray-300 hover:text-gray-100 text-sm font-medium duration-700">개인정보 센터</a>
-<a href="https://withun.link/privacy/tos.html" class="my-3 block text-gray-300 hover:text-gray-100 text-sm font-medium duration-700">이용 약관</a>
-<a href="https://withun.link/privacy/eula.html" class="my-3 block text-gray-300 hover:text-gray-100 text-sm font-medium duration-700">개인정보처리방침</a>
-<a href="https://withun.link/privacy/disclaimer.html" class="my-3 block text-gray-300 hover:text-gray-100 text-sm font-medium duration-700">면책 조항</a>
-</div>
-
-<div class="p-5 w-1/2 sm:w-4/12 md:w-3/12">
-
-<div class="text-xs uppercase text-gray-400 font-medium mb-6">웹 서비스</div>
-
-<a href="https://withun.link/index-parentblock.php" class="my-3 block text-gray-300 hover:text-gray-100 text-sm font-medium duration-700">the switch WARP</a>
-<a href="https://withun.link/index-parentblock.php" class="my-3 block text-gray-300 hover:text-gray-100 text-sm font-medium duration-700">the switch DNS</a>
-</div>
-
+<a href="https://www.apps4flip.com/" class="my-3 block text-gray-800 hover:text-gray-900 text-sm font-medium duration-700">Apps4Flip</a>
+<a href="https://cafe.naver.com/luckyvenusfolder/" class="my-3 block text-gray-800 hover:text-gray-900 text-sm font-medium duration-700"></a>
 
 </div>
+
 
 <div class="pt-2">
 <div class="flex pb-5 px-3 m-auto pt-5 
-            border-t border-gray-500 text-gray-400 text-sm 
+            border-t border-gray-500 text-gray-800 text-sm 
             flex-col md:flex-row max-w-6xl">
 <div class="mt-2">
-© 페이퍼코어 2018~2021 모든 권리 보유. <br><small>페이퍼코어로 발생한 상거래의 책임은 구매자에게 있습니다. 페이퍼코어는 사업자가 아닙니다.</small></div>
+&copy; FPPlay</div>
 
 <div class="md:flex-auto md:flex-row-reverse mt-2 flex-row flex">
 </div>
 </div>
 </div>
 </footer>
-<script async defer src="https://scripts.simpleanalyticscdn.com/latest.js"></script>
-<noscript><img src="https://queue.simpleanalyticscdn.com/noscript.gif" alt=""/></noscript>
