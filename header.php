fe2a10b0420a3681181831093c1b394345e2016d
PaperCore <redev@redevs.ga>
2021-03-27 22:13:04 +0900

    Update header.php

--- header.php
+++ header.php
@@ -1,26 +1,24 @@
 <header class="bg-white shadow-sm w-full fixed z-10  top-0 ">
-        <div class="max-auto  px-8 py-2 bg-white">
-         <div class="flex justify-between">
-		 <a href="https://flipstore.withun.link">
-            <div class="logo flex items-center space-x-4 mr-10">
-              <img class="h-8" src="https://flipstore.withun.link/identity/Group%201.svg" alt="">
-
-                  <h1 class="text-gray-600 text-2xl"></h1>
-            </div>
-		 </a>
-            <div class=" menu flex justify-end  items-center  flex-1 space-x-4">
-<a href="https://withun.link">
-                  <svg  class="h-9 rounded-full px-2 py-2  text-gray-500 hover:bg-gray-200" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6a2 2 0 012-2h2a2 2 0 012 2v2a2 2 0 01-2 2H6a2 2 0 01-2-2V6zM14 6a2 2 0 012-2h2a2 2 0 012 2v2a2 2 0 01-2 2h-2a2 2 0 01-2-2V6zM4 16a2 2 0 012-2h2a2 2 0 012 2v2a2 2 0 01-2 2H6a2 2 0 01-2-2v-2zM14 16a2 2 0 012-2h2a2 2 0 012 2v2a2 2 0 01-2 2h-2a2 2 0 01-2-2v-2z" />
-                  </svg>
+<div class="max-auto  px-4 py-2 bg-white">
+<div class="flex justify-between">
+<a href="https://flipstore.withun.link">
+<div class="logo flex items-center mr-10">
+<img class="h-8" src="https://flipstore.withun.link/identity/Group%201.svg" alt="">
+<h1 class="text-gray-600 text-2xl">Play</h1>
+</div>
 </a>
-                  <img class="h-9 rounded-full border border-gray-100 shadow-sm" src="https://i.stack.imgur.com/34AD2.jpg" alt="user image" />
-            </div>
-         </div>
-        </div>
-    </header>
-
-  <style>
+<div class=" menu flex justify-end  items-center  flex-1 space-x-4">
+<a href="https://withun.link">
+<svg class="h-9 rounded-full px-2 py-2  text-gray-500 hover:bg-gray-200" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+<path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6a2 2 0 012-2h2a2 2 0 012 2v2a2 2 0 01-2 2H6a2 2 0 01-2-2V6zM14 6a2 2 0 012-2h2a2 2 0 012 2v2a2 2 0 01-2 2h-2a2 2 0 01-2-2V6zM4 16a2 2 0 012-2h2a2 2 0 012 2v2a2 2 0 01-2 2H6a2 2 0 01-2-2v-2zM14 16a2 2 0 012-2h2a2 2 0 012 2v2a2 2 0 01-2 2h-2a2 2 0 01-2-2v-2z"></path>
+</svg>
+</a>
+<img class="h-9 rounded-full border border-gray-100 shadow-sm" src="https://i.stack.imgur.com/34AD2.jpg" alt="user image">
+</div>
+</div>
+</div>
+</header>
+<style>
     body{
       background-color:white;
     }
