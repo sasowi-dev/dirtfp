d7e1fffe57c5efc8b20195051687433411938fae
PaperCore <redev@redevs.ga>
2021-03-28 23:33:23 +0900

    Update index.php

--- info/index.php
+++ info/index.php
@@ -8,10 +8,20 @@
 <?php include '../event.php';?>
 <?php include '../header.php';?>
    
-    <p class="text-2xl text-gray-700 font-bold hover:underline">FP Play란</p>
-    <p class="mt-2 text-gray-600">
-국내에서 가장 인기있는(안타깝게도 해외는 아닙니다) 폴더폰 관련 어플리케이션 스토어 입니다.<br>
+      <div class="bg-gray-100 m-auto w-full h-64 mt-5" style="background-image:url('reviewland.png'); background-position: center; background-repeat: no-repeat; background-size: cover;">
+<div class="flex flex-row items-end h-full w-full">
+<div class="flex flex-col w-full pb-3 pt-10 px-3 bg-gradient-to-t from-black text-gray-200">
+<h3 class="text-base font-bold leading-5 uppercase">FP Play란?</h3>
+</div>
+</div>
+</div>
+    <div class="px-2 py-2">
+<div class="rounded bg-blue-100 px-2 py-2 text-gray-700">
+<p>국내에서 가장 인기있는(안타깝게도 해외는 아닙니다) 폴더폰 관련 어플리케이션 스토어 입니다.<br>
     이 스토어에서는 Exalt LTE를 포함한 안드로이드 피처폰들에서 활용할 수 있는 어플리케이션을 배포하고 있습니다.</p>
+</div>
+</div>
+
 <?php include '../footer.php';?>
 </body>
 </html>
