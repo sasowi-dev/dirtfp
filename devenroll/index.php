d238c6cada7099aa4163212c01b82183705d4761
PaperCore <redev@redevs.ga>
2021-04-04 22:02:35 +0900

    Update index.php

--- devenroll/index.php
+++ devenroll/index.php
@@ -6,17 +6,45 @@
   </head>
   <body>
 <?php include '../event.php';?>
-<?php include '../header.php';?>
-<div class="bg-gray-100 m-auto w-full h-64 mt-5" style="background-image:url('reviewland.png'); background-position: center; background-repeat: no-repeat; background-size: cover;">
-<div class="flex flex-row items-end h-full w-full">
-<div class="flex flex-col w-full pb-3 pt-10 px-3 bg-gradient-to-t from-black text-gray-200">
-<h3 class="text-base font-bold leading-5 uppercase">개발자 프로그램 신청</h3>
-</div>
-</div>
-</div>
-    
-    
-    
-<?php include '../footer.php';?>
+<section class="min-h-screen flex flex-col bg-white">
+            <nav>
+                <div class="px-4 py-6">
+<img src="https://flipstore.withun.link/identity/Group%201.svg">
+                </div>
+            </nav>
+            <div class="flex flex-1 items-center justify-center">
+                <div class="rounded-lg sm:border-2 px-4 lg:px-24 py-16 lg:max-w-xl sm:max-w-md w-full text-center">
+                    <div class="text-center">
+                        <h1 class="font-bold text-3xl mb-8 w-full text-gray-600">
+                           통신사 선택 / Select Carrier
+                            <p class="font-light text-gray-500 text-xs">FP Play에서 다양한 어플리케이션, 게임을 다운로드하고 플레이하세요.<br>
+                            Enjoy unlimited applications and games from FP Play.</p>
+
+                        </h1>
+                        <div class="py-2">
+                          <!--Verizon-->
+                            <button class=" border-2 border-gray-100 focus:outline-none bg-white-600 text-black font-bold block w-full p-2 rounded-full shadow-md ">
+                            Verizon                              
+                            </button>
+                            <br>
+                          <!--LGU-->
+                            <button class="border-2 border-gray-100 focus:outline-none bg-white text-pink-600 font-bold block w-full p-2 rounded-full shadow-md ">
+                            LG U+
+                            </button>
+                            <br>
+                          <!--KT-->
+                                                      <button class=" border-2 border-gray-100 focus:outline-none bg-white-600 text-red-500 font-bold block w-full p-2 rounded-full shadow-md ">
+                            KT                             
+                            </button><br>
+                            <!--SKT-->
+                                                        <button class=" border-2 border-gray-100 focus:outline-none bg-white-600 text-yellow-700 font-bold block w-full p-2 rounded-full shadow-md ">
+                            SKT                          
+                            </button>
+                        </div>
+                    </div>
+                  
+                </div>
+            </div>
+        </section>
 </body>
 </html>
