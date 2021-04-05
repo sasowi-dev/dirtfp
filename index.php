40f47e9a79e6b10ef6bcd02af17f681feb94ee5a
PaperCore <redev@redevs.ga>
2021-04-05 09:07:59 +0900

    Update index.php

--- index.php
+++ index.php
@@ -10,29 +10,7 @@
 
     <div class="">
        <!--FP이외 제한구역-->
-<div class="bg-gray-100 m-auto w-full h-64 mt-5" style="background-image:url('https://play-lh.googleusercontent.com/zKs_WRjncpVdsxo2Ifwjsxg0aupC2du9g5ECf-2pJ_FpHDZCRGJIrTMCnvlAKRoQhj3g9VUtXik=w490-h245-rw'); background-position: center; background-repeat: no-repeat; background-size: cover;">
-<div class="flex flex-row items-end h-full w-full">
-<div class="flex flex-col w-full pb-3 pt-10 px-3 bg-gradient-to-t from-black text-gray-200">
-<h3 class="text-base font-bold leading-5 uppercase">어서 오세요!</h3>
-<div class="inline-flex items-center">
-<span class="capitalize font-base text-xs my-1 mr-1">FPPlay 어드민</span>
-<svg class="stroke-current stroke-1 text-blue-600 w-4 verified" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor">
-<path fill-rule="evenodd" d="M6.267 3.455a3.066 3.066 0 001.745-.723 3.066 3.066 0 013.976 0 3.066 3.066 0 001.745.723 3.066 3.066 0 012.812 2.812c.051.643.304 1.254.723 1.745a3.066 3.066 0 010 3.976 3.066 3.066 0 00-.723 1.745 3.066 3.066 0 01-2.812 2.812 3.066 3.066 0 00-1.745.723 3.066 3.066 0 01-3.976 0 3.066 3.066 0 00-1.745-.723 3.066 3.066 0 01-2.812-2.812 3.066 3.066 0 00-.723-1.745 3.066 3.066 0 010-3.976 3.066 3.066 0 00.723-1.745 3.066 3.066 0 012.812-2.812zm7.44 5.252a1 1 0 00-1.414-1.414L9 10.586 7.707 9.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l4-4z" clip-rule="evenodd"></path>
-</svg>
-</div>
-<div class="flex flex-row justify-between">
-<div class="flex flex-row">
-<div class="w-max inline-flex items-center">
-<svg class="w-4" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-<path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6a2 2 0 012-2h2a2 2 0 012 2v2a2 2 0 01-2 2H6a2 2 0 01-2-2V6zM14 6a2 2 0 012-2h2a2 2 0 012 2v2a2 2 0 01-2 2h-2a2 2 0 01-2-2V6zM4 16a2 2 0 012-2h2a2 2 0 012 2v2a2 2 0 01-2 2H6a2 2 0 01-2-2v-2zM14 16a2 2 0 012-2h2a2 2 0 012 2v2a2 2 0 01-2 2h-2a2 2 0 01-2-2v-2z"></path>
-</svg>
-<span class="text-xs ml-1 antialiased">키패드 ①번 선택 <small>또는 터치</small></span>
-</div>
-</div>
-</div>
-</div>
-</div>
-</div>
+
       <?php
   try {
   $con= new PDO('mysql:host=localhost:3306;dbname=fpstre', "chocolatekos", "leegyubin!Lee06");
