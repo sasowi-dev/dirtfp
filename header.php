3b9bf5c8bc7cbf6f6e965c62ec4d070123390f50
PaperCore <redev@redevs.ga>
2021-04-05 11:46:04 +0900

    Update header.php

--- header.php
+++ header.php
@@ -1,7 +1,7 @@
 <header class="bg-white shadow-sm w-full z-10 fixed top-0 ">
 <div class="max-auto  px-4 py-2 bg-white">
 <div class="flex justify-between">
-<a href="https://flipstore.withun.link/app">
+<a href="https://flipstore.withun.link/">
 <div class="logo flex items-center mr-10">
 <img class="h-8" src="https://flipstore.withun.link/identity/Group%201.svg" alt="">
 <h1 class="text-gray-600 text-2xl" id="product-sans">FP Play</h1>
