83c11609c3fbd1ec5848f382b4d5108fd616a51f
PaperCore <redev@redevs.ga>
2021-04-05 10:49:47 +0900

    Update index.php

--- index.php
+++ index.php
@@ -38,6 +38,122 @@ var glideHeroPeek = new Glide('.heropeek', {
 
 glideHeroPeek.mount();
       </script>
+      	<script>
+		const modal = document.querySelector('.main-modal');
+		const closeButton = document.querySelectorAll('.modal-close');
+
+		const modalClose = () => {
+			modal.classList.remove('fadeIn');
+			modal.classList.add('fadeOut');
+			setTimeout(() => {
+				modal.style.display = 'none';
+			}, 500);
+		}
+
+		function openModal(amnt,app) {
+			document.getElementById("payamount").innerHTML = amnt;
+			document.getElementById("prodnam").innerHTML = app;
+			modal.classList.remove('fadeOut');
+			modal.classList.add('fadeIn');
+			modal.style.display = 'flex';
+		}
+
+		for (let i = 0; i < closeButton.length; i++) {
+
+			const elements = closeButton[i];
+
+			elements.onclick = (e) => modalClose();
+
+			modal.style.display = 'none';
+
+			window.onclick = function (event) {
+				if (event.target == modal) modalClose();
+			}
+		}
+	</script>
+  
+	<div class="main-modal fixed w-full h-100 inset-0 z-50 overflow-hidden flex justify-center items-center animated fadeIn faster"
+		style="background: rgba(0,0,0,.7);">
+		<div
+			class="border border-teal-500 shadow-lg modal-container bg-white w-11/12 md:max-w-md mx-auto rounded shadow-lg z-50 overflow-y-auto">
+			<div class="modal-content py-4 text-left px-6">
+				<!--Title-->
+				<div class="flex justify-between items-center pb-3">
+					<p class="text-2xl font-bold">결제 (<span class="text-blue-700" id="payamount"></span>원)</p>
+					<div class="modal-close cursor-pointer z-50">
+						<svg class="fill-current text-black" xmlns="http://www.w3.org/2000/svg" width="18" height="18"
+							viewBox="0 0 18 18">
+							<path
+								d="M14.53 4.53l-1.06-1.06L9 7.94 4.53 3.47 3.47 4.53 7.94 9l-4.47 4.47 1.06 1.06L9 10.06l4.47 4.47 1.06-1.06L10.06 9z">
+							</path>
+						</svg>
+					</div>
+				</div>
+				<!--Body-->
+				<div class="my-5">
+					<div class="">
+<div class="max-w-4xl  bg-white rounded-lg">
+<div class="mt-2"><p class="text-2xl text-gray-700 font-bold hover:underline" id="prodnam"></p>
+</div>
+</div>
+</div>
+					<p class="text-xs text-gray-500 py-2 tracking-wider">결제 수단</p>
+					<figure class="flex rounded-xl  p-0">
+  <div class="text-left ">
+    <blockquote>
+    </blockquote>
+    <figcaption class="font-medium">
+      <div class="text-gray-500">
+       payments-fp@withun.link로 Google Play에서 구매한 어플리케이션의 영수증 사본을 전송하세요.
+      </div>
+    </figcaption>
+  </div>
+</figure>
+				</div>
+				<!--Footer-->
+				<div class="flex justify-end pt-2">
+								</div>
+			</div>
+		</div>
+	</div>
+ <style>
+		.animated {
+			-webkit-animation-duration: 500ms;
+			animation-duration: 500ms;
+			-webkit-animation-fill-mode: both;
+			animation-fill-mode: both;
+		}
+
+		.fadeIn {
+			-webkit-animation-name: fadeIn;
+			animation-name: fadeIn;
+		}
+
+		.fadeOut {
+			-webkit-animation-name: fadeOut;
+			animation-name: fadeOut;
+		}
+
+		@keyframes fadeIn {
+			from {
+				opacity: 0;
+			}
+
+			to {
+				opacity: 1;
+			}
+		}
+
+		@keyframes fadeOut {
+			from {
+				opacity: 1;
+			}
+
+			to {
+				opacity: 0;
+			}
+		}
+	</style>
       <?php
   try {
   $con= new PDO('mysql:host=localhost:3306;dbname=fpstre', "chocolatekos", "leegyubin!Lee06");
