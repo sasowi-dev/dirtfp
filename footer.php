ece7124800417ebc13fc6019838ba38fefea58de
PaperCore <redev@redevs.ga>
2021-04-05 10:14:39 +0900

    Update footer.php

--- footer.php
+++ footer.php
@@ -1,12 +1,1 @@
-<footer class="bg-white pt-10 sm:mt-10 pt-10">
-<div class="max-w-6xl m-auto text-gray-800 flex flex-wrap justify-left">
 
-<div class="p-5 w-full sm:w-4/12 md:w-3/12">
-
-<div class="text-xs uppercase text-gray-800 font-medium mb-6">추천</div>
-
-<a href="https://www.apps4flip.com/" class="my-3 block text-gray-800 hover:text-gray-900 text-sm font-medium duration-700">Apps4Flip</a>
-<a href="https://cafe.naver.com/luckyvenusfolder/" class="my-3 block text-gray-800 hover:text-gray-900 text-sm font-medium duration-700">네이버 카페</a>
-
-</div>
-</footer>
