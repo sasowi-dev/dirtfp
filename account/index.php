64ce5c6c3f0d494c062da98ff9e04bf64edeb2cf
PaperCore <redev@redevs.ga>
2021-04-05 10:14:08 +0900

    Update index.php

--- account/index.php
+++ account/index.php
@@ -11,7 +11,7 @@
 
 <div class="mt-5">
 <div class="bg-blue-50 mr-50 rounded rounded-r-3xl pl-6 py-3 font-semibold">
-<button class="text-blue-500 text-sm font-semibold flex items-center  focus:outline-none">
+<button class="text-blue-500 text-sm font-semibold flex items-center  focus:outline-none heroprod">
   <svg class="h-5  px-3" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
   <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 16v1a3 3 0 003 3h10a3 3 0 003-3v-1m-4-4l-4 4m0 0l-4-4m4 4V4" />
 </svg>어플리케이션</button>
@@ -27,10 +27,12 @@ Trash</button>
 </div>-->
 <hr class="my-2">
 <div class="mt-1  mr-50 rounded rounded-r-3xl pl-6 py-3 font-semibold">
-<button class="text-gray-600  text-sm font-semibold flex items-center focus:outline-none">
+  <a href="https://cafe.naver.com/luckyvenusfolder">
+<button class="text-gray-600  text-sm font-semibold flex items-center focus:outline-none heroprod">
   <svg class="h-6  px-3" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
   <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M8 12h.01M12 12h.01M16 12h.01M21 12c0 4.418-4.03 8-9 8a9.863 9.863 0 01-4.255-.949L3 20l1.395-3.72C3.512 15.042 3 13.574 3 12c0-4.418 4.03-8 9-8s9 3.582 9 8z" />
 </svg>Secret Play</button>
+  </a>
 </div>
 </div>
 </aside>
