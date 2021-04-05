8b7e4d0708fd63befa2e72ef1e5fa7e2eac396f1
PaperCore <redev@redevs.ga>
2021-04-05 10:13:02 +0900

    Update index.php

--- account/index.php
+++ account/index.php
@@ -7,61 +7,33 @@
   <body>
 <?php include '../event.php';?>
 <?php include '../header.php';?>
-  <div class="mt-16">
-<aside class="col-span-4 bg-white h-screen py-5  w-60">
-        <button class="flex items-center align-middle py-2 bg-white shadow-md  rounded-3xl text-gray-800 text-sm font-semibold ml-3 border border-gray-200 hover:shadow-xl transition-all w-36  focus:outline-none">   <svg class="h-8 px-4" viewBox="0 0 36 36"><path class="ng-tns-c17-1" d="M16 16v14h4V20z" fill="#34A853"></path><path class="ng-tns-c17-1" d="M30 16H20l-4 4h14z" fill="#4285F4"></path><path class="ng-tns-c17-1" d="M6 16v4h10l4-4z" fill="#FBBC05"></path><path class="ng-tns-c17-1" d="M20 16V6h-4v14z" fill="#EA4335"></path><path class="ng-tns-c17-1" d="M0 0h36v36H0z" fill="none"></path></svg>Create</button>
+  <div class="mt-16"><aside class="col-span-4 bg-white py-5  w-60">
+
+<div class="mt-5">
+<div class="bg-blue-50 mr-50 rounded rounded-r-3xl pl-6 py-3 font-semibold">
+<button class="text-blue-500 text-sm font-semibold flex items-center  focus:outline-none">
+  <svg class="h-5  px-3" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+  <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 16v1a3 3 0 003 3h10a3 3 0 003-3v-1m-4-4l-4 4m0 0l-4-4m4 4V4" />
+</svg>어플리케이션</button>
+</div>
 
 
-        <div class="mt-5">
-          <div class="bg-blue-50 mr-50 rounded rounded-r-3xl pl-6 py-3 font-semibold">
-            <button class="text-blue-500 text-sm font-semibold flex items-center  focus:outline-none">
-              <svg class="h-5  px-3" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 8v4l3 3m6-3a9 9 0 11-18 0 9 9 0 0118 0z"></path>
-              </svg>
-              Recent</button>
-              
-           </div>
-           <div class="mt-1  mr-50 rounded rounded-r-3xl pl-6 py-3 font-semibold">
-            <button class="text-gray-600  text-sm font-semibold flex items-center  focus:outline-none">
-              <svg class="h-6  px-3" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 4.354a4 4 0 110 5.292M15 21H3v-1a6 6 0 0112 0v1zm0 0h6v-1a6 6 0 00-9-5.197M13 7a4 4 0 11-8 0 4 4 0 018 0z"></path>
-              </svg>
-              Shared with me</button>
-              
-           </div>
-
-           <div class="mt-1  mr-50 rounded rounded-r-3xl pl-6 py-3 font-semibold">
-            <button class="text-gray-600 text-sm font-semibold flex items-center  focus:outline-none">
-              <svg class="h-6  px-3" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M16 7a4 4 0 11-8 0 4 4 0 018 0zM12 14a7 7 0 00-7 7h14a7 7 0 00-7-7z"></path>
-              </svg>
-              Owned by me</button>
-              
-           </div>
-
-         
-           <div class="mt-1  mr-50 rounded rounded-r-3xl pl-6 py-3 font-semibold">
-            <button class="text-gray-600  text-sm font-semibold flex items-center focus:outline-none">
-              <svg class="h-6  px-3" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M19 7l-.867 12.142A2 2 0 0116.138 21H7.862a2 2 0 01-1.995-1.858L5 7m5 4v6m4-6v6m1-10V4a1 1 0 00-1-1h-4a1 1 0 00-1 1v3M4 7h16"></path>
-              </svg>
-          Trash</button>
-              
-           </div>
-
-  <hr class="my-2">
-  <div class="mt-1  mr-50 rounded rounded-r-3xl pl-6 py-3 font-semibold">
-    <button class="text-gray-600  text-sm font-semibold flex items-center focus:outline-none">
-      <svg class="h-6  px-3" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M8 14v3m4-3v3m4-3v3M3 21h18M3 10h18M3 7l9-4 9 4M4 10h16v11H4V10z"></path>
-      </svg>
-  Enterprise Admin</button>
-
-
-      
-   </div>
-        </div>
-      </aside>
+<!--<div class="mt-1  mr-50 rounded rounded-r-3xl pl-6 py-3 font-semibold">
+<button class="text-gray-600  text-sm font-semibold flex items-center focus:outline-none">
+<svg class="h-6  px-3" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+<path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M19 7l-.867 12.142A2 2 0 0116.138 21H7.862a2 2 0 01-1.995-1.858L5 7m5 4v6m4-6v6m1-10V4a1 1 0 00-1-1h-4a1 1 0 00-1 1v3M4 7h16"></path>
+</svg>
+Trash</button>
+</div>-->
+<hr class="my-2">
+<div class="mt-1  mr-50 rounded rounded-r-3xl pl-6 py-3 font-semibold">
+<button class="text-gray-600  text-sm font-semibold flex items-center focus:outline-none">
+  <svg class="h-6  px-3" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+  <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M8 12h.01M12 12h.01M16 12h.01M21 12c0 4.418-4.03 8-9 8a9.863 9.863 0 01-4.255-.949L3 20l1.395-3.72C3.512 15.042 3 13.574 3 12c0-4.418 4.03-8 9-8s9 3.582 9 8z" />
+</svg>Secret Play</button>
+</div>
+</div>
+</aside>
 </div>
 <?php include '../footer.php';?>
 </body>
