19a562a93fa8187aab1e8e274fb0696fc20cc8d5
PaperCore <redev@redevs.ga>
2021-04-05 11:04:44 +0900

    Create index.php

---
+++ index.php
@@ -0,0 +1,36 @@
+<!DOCTYPE HTML>
+<html lang="ko">
+<head>
+<meta charset="utf-8">
+<?php include 'dependencies.php';?>
+  </head>
+  <body>
+<?php include 'event.php';?>
+   <section class="min-h-screen flex flex-col bg-white">
+<nav>
+<div class="px-4 py-6">
+<img src="https://flipstore.withun.link/identity/Group%201.svg">
+</div>
+</nav>
+<div class="flex flex-1 items-center justify-center">
+<div class="rounded-lg sm:border-2 px-4 lg:px-24 py-16 lg:max-w-xl sm:max-w-md w-full text-center">
+<div class="text-center">
+<h1 class="font-bold text-3xl mb-8 w-full text-gray-600">
+어플리케이션 다운로드 / Download Application
+<p class="font-light text-gray-500 text-xs">앱으로 FP Play에서 다양한 어플리케이션, 게임을 다운로드하고 플레이하세요.<br>
+Enjoy unlimited applications and games from FP Play App.</p>
+</h1>
+<div class="py-2">
+<a href="https://downapi.cafe.naver.com/v1.0/cafes/article/file/9846d6d4-8fd4-11eb-9ffa-0050568d055f/download">
+<button class=" border-2 border-gray-100 focus:outline-none bg-white-600 text-black font-bold block w-full p-2 rounded-full shadow-md ">
+1.2 다운로드
+</button>
+  </a>
+</div>
+</div>
+</div>
+</div>
+</section>
+<?php include 'footer.php';?>
+</body>
+</html>
