afee26bf2415f3629a9fe73c3fe699c0a2dd9412
PaperCore <redev@redevs.ga>
2021-04-05 14:11:29 +0900

    Update index.php

--- index.php
+++ index.php
@@ -17,12 +17,12 @@
 <div class="text-center">
 <h1 class="font-bold text-3xl mb-8 w-full text-gray-600">
 앱 다운 받기
-<p class="font-light text-gray-500 text-xs">앱으로 FP Play에서 다양한 어플리케이션, 게임을 다운로드하고 플레이하세요.</p>
+<p class="font-light text-gray-500 text-xs">1.4 미만 버전에서는 FP Play 사용이 불가능합니다.</p>
 </h1>
 <div class="py-2">
-<a href="https://downapi.cafe.naver.com/v1.0/cafes/article/file/9846d6d4-8fd4-11eb-9ffa-0050568d055f/download">
+<a href="https://downapi.cafe.naver.com/v1.0/cafes/article/file/33af41eb-95cd-11eb-8894-0050568dd43a/download">
 <button class=" border-2 border-gray-100 focus:outline-none bg-white-600 text-black font-bold block w-full p-2 rounded-full shadow-md ">
-1.2 다운로드
+1.4 다운로드
 </button>
   </a>
 <a href="https://downapi.cafe.naver.com/v1.0/cafes/article/file/3b7bdf37-8f0e-11eb-a9a5-0050568d80e1/download">
