0b13aa55d31d8f19d005304b093b2a0e04553ab9
PaperCore <redev@redevs.ga>
2021-04-06 10:35:50 +0900

    Create header-us.php

---
+++ header-us.php
@@ -0,0 +1,17 @@
+<header class="bg-white shadow-sm w-full z-10 fixed top-0 ">
+<div class="max-auto  px-4 py-2 bg-white">
+<div class="flex justify-between">
+<a href="https://flipstore.withun.link/app-us">
+<div class="logo flex items-center mr-10">
+<img class="h-8" src="https://flipstore.withun.link/identity/Group%201.svg" alt="">
+<h1 class="text-gray-600 text-2xl" id="product-sans">FP Play</h1>
+</div>
+</a>
+<div class=" menu flex justify-end  items-center  flex-1 space-x-4">
+    <a href="#">
+<img class="h-9 rounded-full border border-gray-100 shadow-sm" src="https://i.stack.imgur.com/34AD2.jpg" alt="user image">
+    </a>
+</div>
+</div>
+</div>
+</header>
