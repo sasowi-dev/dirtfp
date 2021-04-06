3c8b91ca296120ad1da342fc34a595548387a1cd
PaperCore <redev@redevs.ga>
2021-04-06 11:52:04 +0900

    Create twitter.php

---
+++ twitter.php
@@ -0,0 +1,13 @@
+<!DOCTYPE HTML>
+<html lang="ko">
+<head>
+<meta charset="utf-8">
+<?php include 'dependencies-en.php';?>
+  </head>
+  <body>
+<?php include 'event-en.php';?>
+<?php include 'header-en.php';?>
+    <a class="twitter-timeline" href="https://twitter.com/PaperCoreDev?ref_src=twsrc%5Etfw">Tweets by PaperCoreDev</a> <script async src="https://platform.twitter.com/widgets.js" charset="utf-8"></script>
+<?php include 'footer-en.php';?>
+</body>
+</html>
