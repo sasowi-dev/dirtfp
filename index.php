ccf735ccf978f4e5f2da974290a09d4b0b52a79c
PaperCore <redev@redevs.ga>
2021-04-06 11:04:13 +0900

    Update index.php

--- index.php
+++ index.php
@@ -17,17 +17,22 @@
 <div class="text-center">
 <h1 class="font-bold text-3xl mb-8 w-full text-gray-600">
 Download application
-<p class="font-light text-gray-500 text-xs">FP Play app below version 1.4 cannot access.<br>FP Play application has US and KR Varient.<br>Select in your choice.</p>
+<p class="font-light text-gray-500 text-xs">Download your locale's varient.<br>당신의 언어에 맞는 앱을 다운로드하세요.</p>
 </h1>
 <div class="py-2">
 <a href="https://downapi.cafe.naver.com/v1.0/cafes/article/file/33af41eb-95cd-11eb-8894-0050568dd43a/download">
 <button class=" border-2 border-gray-100 focus:outline-none bg-white-600 text-black font-bold block w-full p-2 rounded-full shadow-md ">
-대한민국전용 - 1.4 다운로드
+[한국어] - 1.4
+</button>
+  </a>
+ <a href="https://downapi.cafe.naver.com/v1.0/cafes/article/file/33af41eb-95cd-11eb-8894-0050568dd43a/download">
+<button class=" border-2 border-gray-100 focus:outline-none bg-white-600 text-black font-bold block w-full p-2 rounded-full shadow-md ">
+[English] - 1.4
 </button>
   </a>
 <a href="https://downapi.cafe.naver.com/v1.0/cafes/article/file/3b7bdf37-8f0e-11eb-a9a5-0050568d80e1/download">
 <button class=" border-2 border-gray-100 focus:outline-none bg-white-600 text-black font-bold block w-full p-2 rounded-full shadow-md ">
-Download Browsing Engine<br>브라우징 엔진 
+Chrome (필수/Mandatory)
 </button>
   </a>
 </div>
