460172bfb64db688814c9ebef5bb6491be3715b2
PaperCore <redev@redevs.ga>
2021-04-06 13:06:33 +0900

    Update index.php

--- index.php
+++ index.php
@@ -2,44 +2,96 @@
 <html lang="ko">
 <head>
 <meta charset="utf-8">
-<?php include 'dependencies.php';?>
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<link rel="apple-touch-icon" sizes="180.180" href="https://flipstore.withun.link/identity/apple-touch-icon.png">
+<link rel="icon" type="image/png" sizes="192x192"  href="https://flipstore.withun.link/identity/android-chrome-192x192.png">
+<link rel="icon" type="image/png" sizes="32x32" href="https://flipstore.withun.link/identity/favicon-32x32.png">
+<link rel="icon" type="image/png" sizes="16x16" href="https://flipstore.withun.link/identity/favicon-16x16.png">
+<link rel="manifest" href="https://flipstore.withun.link/identity/site.webmanifest">
+<meta name="msapplication-TileColor" content="#1D4ED8">
+<meta name="theme-color" content="#1D4ED8">
+<title>Flip Play</title>
+<meta name="title" content="Flip Play">
+<meta name="description" content="Application store for 4G Flip phones, now for Exalt LTE.">
+<meta property="og:type" content="website">
+<meta property="og:url" content="https://flipstore.withun.link/">
+<meta property="og:title" content="Flip Play">
+<meta property="og:description" content="Application store for 4G Flip phones, now for Exalt LTE.">
+<meta property="og:image" content="https://flipstore.withun.link/identity/papercore_new.png">
+<meta property="twitter:card" content="summary_large_image">
+<meta property="twitter:url" content="https://flipstore.withun.link/">
+<meta property="twitter:title" content="Flip Play">
+<meta property="twitter:description" content="Application store for 4G Flip phones, now for Exalt LTE.">
+<meta property="twitter:image" content="https://flipstore.withun.link/identity/papercore_new.png">
+<link href="https://cdnjs.cloudflare.com/ajax/libs/tailwindcss/2.0.3/tailwind.min.css" rel="stylesheet">
+<style>
+/* NotoSans 설정*/
+@import url(//fonts.googleapis.com/earlyaccess/notosanskr.css);
+/* Google Product Sans 설정 */
+@font-face {
+  font-family: 'Product Sans';
+  font-style: normal;
+  font-weight: 400;
+  src: local('Open Sans'), local('OpenSans'), url(https://fonts.gstatic.com/s/productsans/v5/HYvgU2fE2nRJvZ5JFAumwegdm0LZdjqr5-oayXSOefg.woff2) format('woff2');
+}
+/*로고 부분*/
+ #product-sans
+  {
+    font-family: 'Product Sans';
+  }
+/*전체 노토 적용*/
+* { 
+ font-family: 'Noto Sans KR', sans-serif;
+}
+/*프로덕트*/
+  .heroprod{
+     font-family: 'Product Sans','Noto Sans KR', sans-serif;
+  }
+  body{
+    background-color:white;
+  }
+</style>
   </head>
   <body>
-<?php include 'event.php';?>
-   <section class="min-h-screen flex flex-col bg-white">
-<nav>
-<div class="px-2 py-3">
-<img src="https://flipstore.withun.link/identity/Group%201.svg">
+<header class="bg-white shadow-sm w-full z-10 fixed top-0 ">
+<div class="max-auto  px-4 py-2 bg-white">
+<div class="flex justify-between">
+<a href="#">
+<div class="logo flex items-center mr-10">
+<img class="h-8" src="https://flipstore.withun.link/identity/Group%201.svg" alt="">
+<h1 class="text-gray-600 text-2xl" id="product-sans">Flip Play</h1>
 </div>
-</nav>
-<div class="flex flex-1 items-center justify-center">
-<div class="rounded-lg sm:border-2 px-4 lg:px-24 py-16 lg:max-w-xl sm:max-w-md w-full text-center">
-<div class="text-center">
-<h1 class="font-bold text-3xl mb-8 w-full text-gray-600">
-Download application
-<p class="font-light text-gray-500 text-xs">Download your locale's varient.<br>당신의 언어에 맞는 앱을 다운로드하세요.</p>
-</h1>
-<div class="py-2">
-<a href="https://downapi.cafe.naver.com/v1.0/cafes/article/file/33af41eb-95cd-11eb-8894-0050568dd43a/download">
-<button class=" border-2 border-gray-100 focus:outline-none bg-white-600 text-black font-bold block w-full rounded-full shadow-md ">
-[한국어] - 1.4
-</button>
-  </a>
- <a href="https://downapi.cafe.naver.com/v1.0/cafes/article/file/33af41eb-95cd-11eb-8894-0050568dd43a/download">
-<button class=" border-2 border-gray-100 focus:outline-none bg-white-600 text-black font-bold block w-full rounded-full shadow-md ">
-[English] - 1.4
-</button>
-  </a>
-<a href="https://downapi.cafe.naver.com/v1.0/cafes/article/file/3b7bdf37-8f0e-11eb-a9a5-0050568d80e1/download">
-<button class=" border-2 border-gray-100 focus:outline-none bg-white-600 text-black font-bold block w-full rounded-full shadow-md ">
-Chrome (Req, 필수)
-</button>
-  </a>
+</a>
+<div class=" menu flex justify-end  items-center  flex-1 space-x-4">
+    <a href="https://twitter.com/PaperCoreDev">
+<img class="h-9 rounded-full border border-gray-100 shadow-sm" src="https://flipstore.withun.link/124021.png" alt="Twitter">
+    </a>
 </div>
 </div>
 </div>
+</header>
+    
+    <div class="bg-gray-100">
+   <div class="max-w-6xl m-auto text-gray-800 flex flex-wrap justify-center">
+      <div class="p-5 w-48 ">
+         <div class="text-xs uppercase text-gray-500 font-medium">Recommended</div>
+         <a class="my-3 block" href="https://Apps4Flip.com">Apps4Flip<span class="text-teal-600 text-xs p-1"></span></a><a class="my-3 block" href="https://github.com/sh7411usa/VirtualFlip">VirtualFlip <span class="text-teal-600 text-xs p-1"></span></a> 
+      </div>
+      <div class="p-5 w-48 ">
+         <div class="text-xs uppercase text-gray-500 font-medium">Download</div>
+         <a class="my-3 block" href="https://downapi.cafe.naver.com/v1.0/cafes/article/file/33af41eb-95cd-11eb-8894-0050568dd43a/download">App (한국어, 1.4)<span class="text-teal-600 text-xs p-1"></span></a><a class="my-3 block" href="/#">App (English, 1.4) <span class="text-teal-600 text-xs p-1"></span></a><a class="my-3 block" href="https://appetize.io/embed/4p9kuny8bx0hu7zkgp0qchuezc?device=nexus5&scale=75&orientation=portrait&osVersion=10.0&screenOnly=true&timezone=Asia%2FSeoul&androidPackageManager=true&centered=true">Online Simulator <span class="text-teal-600 text-xs p-1">New</span></a>
+      </div>
+
+   </div>
 </div>
-</section>
-<?php include 'footer.php';?>
+
+<div class="bg-gray-100 pt-2 ">
+   <div class="flex pb-5 px-3 m-auto pt-5 border-t text-gray-800 text-sm flex-col
+      md:flex-row max-w-6xl">
+      <div class="mt-2">© Copyright 2021 PaperCore. All rights reserved.</div>
+      <div class="md:flex-auto md:flex-row-reverse mt-2 flex-row flex">
+      </div>
+   </div>
+</div>
 </body>
 </html>
