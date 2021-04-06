4d2d10a0dd703607a9e6d6f1982146f6121eefdd
PaperCore <redev@redevs.ga>
2021-04-06 11:01:38 +0900

    Update index.php

--- index.php
+++ index.php
@@ -16,18 +16,18 @@
 <div class="rounded-lg sm:border-2 px-4 lg:px-24 py-16 lg:max-w-xl sm:max-w-md w-full text-center">
 <div class="text-center">
 <h1 class="font-bold text-3xl mb-8 w-full text-gray-600">
-앱 다운 받기
-<p class="font-light text-gray-500 text-xs">1.4 미만 버전에서는 FP Play 사용이 불가능합니다.</p>
+Download application
+<p class="font-light text-gray-500 text-xs">FP Play app below version 1.4 cannot access.<br>FP Play application has US and KR Varient.<br>Select in your choice.</p>
 </h1>
 <div class="py-2">
 <a href="https://downapi.cafe.naver.com/v1.0/cafes/article/file/33af41eb-95cd-11eb-8894-0050568dd43a/download">
 <button class=" border-2 border-gray-100 focus:outline-none bg-white-600 text-black font-bold block w-full p-2 rounded-full shadow-md ">
-1.4 다운로드
+대한민국전용 - 1.4 다운로드
 </button>
   </a>
 <a href="https://downapi.cafe.naver.com/v1.0/cafes/article/file/3b7bdf37-8f0e-11eb-a9a5-0050568d80e1/download">
 <button class=" border-2 border-gray-100 focus:outline-none bg-white-600 text-black font-bold block w-full p-2 rounded-full shadow-md ">
-브라우저 다운로드
+Download Browsing Engine<br>브라우징 엔진 
 </button>
   </a>
 </div>
