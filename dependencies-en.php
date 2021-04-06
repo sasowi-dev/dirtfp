27b36077c9e5c0523990c02f7382de803b537748
PaperCore <redev@redevs.ga>
2021-04-06 10:34:44 +0900

    Create dependencies-en.php

---
+++ dependencies-en.php
@@ -0,0 +1,51 @@
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<link rel="apple-touch-icon" sizes="180.180" href="https://flipstore.withun.link/identity/apple-touch-icon.png">
+<link rel="icon" type="image/png" sizes="192x192"  href="https://flipstore.withun.link/identity/android-chrome-192x192.png">
+<link rel="icon" type="image/png" sizes="32x32" href="https://flipstore.withun.link/identity/favicon-32x32.png">
+<link rel="icon" type="image/png" sizes="16x16" href="https://flipstore.withun.link/identity/favicon-16x16.png">
+<link rel="manifest" href="https://flipstore.withun.link/identity/site.webmanifest">
+<meta name="msapplication-TileColor" content="#1D4ED8">
+<meta name="theme-color" content="#1D4ED8">
+<title>FP Play</title>
+<meta name="title" content="FP Play">
+<meta name="description" content="Application store for flip phones.">
+<meta property="og:type" content="website">
+<meta property="og:url" content="https://flipstore.withun.link/">
+<meta property="og:title" content="FP Play">
+<meta property="og:description" content="Application store for flip phones.">
+<meta property="og:image" content="https://flipstore.withun.link/identity/papercore_new.png">
+<meta property="twitter:card" content="summary_large_image">
+<meta property="twitter:url" content="https://flipstore.withun.link/">
+<meta property="twitter:title" content="FP Play">
+<meta property="twitter:description" content="Application store for flip phones.">
+<meta property="twitter:image" content="https://flipstore.withun.link/identity/papercore_new.png">
+<link href="https://cdnjs.cloudflare.com/ajax/libs/tailwindcss/2.0.3/tailwind.min.css" rel="stylesheet">
+<script src="https://cdn.jsdelivr.net/npm/@glidejs/glide"></script>
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/@glidejs/glide/dist/css/glide.core.min.css">
+
+<style>
+  @import url('https://fonts.googleapis.com/css2?family=Roboto&display=swap');
+/* Google Product Sans 설정 */
+@font-face {
+  font-family: 'Product Sans';
+  font-style: normal;
+  font-weight: 400;
+  src: local('Open Sans'), local('OpenSans'), url(https://fonts.gstatic.com/s/productsans/v5/HYvgU2fE2nRJvZ5JFAumwegdm0LZdjqr5-oayXSOefg.woff2) format('woff2');
+}
+/*Logo Area*/
+ #product-sans
+  {
+    font-family: 'Product Sans';
+  }
+/*Global Noto Sans*/
+* { 
+ font-family: 'Roboto', sans-serif;
+}
+/*Product Sans*/
+  .heroprod{
+     font-family: 'Product Sans', sans-serif;
+  }
+  body{
+    background-color:white;
+  }
+</style>
