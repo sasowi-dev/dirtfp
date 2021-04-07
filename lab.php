57ba853914ec0d6bc2575dc18bbc91202c5a40be
PaperCore <redev@redevs.ga>
2021-04-07 09:01:30 +0900

    Create lab.php

---
+++ lab.php
@@ -0,0 +1,76 @@
+<!DOCTYPE HTML>
+<html lang="ko">
+<head>
+  <meta charset="utf-8">
+<?php include 'dependencies-en.php';?>
+  </head>
+  <body>
+<header class="bg-white shadow-sm w-full z-10 fixed top-0 ">
+<div class="max-auto  px-4 py-2 bg-white">
+<div class="flex justify-between">
+<a href="#">
+<div class="logo flex items-center mr-10">
+<img class="h-8" src="https://flipstore.withun.link/identity/Group%201.svg" alt="">
+<h1 class="text-gray-600 text-2xl" id="product-sans">Flip Play</h1>
+</div>
+</a>
+<div class=" menu flex justify-end  items-center  flex-1 space-x-4">
+    <a href="https://twitter.com/PaperCoreDev">
+<img class="h-9 rounded-full border border-gray-100 shadow-sm" src="https://flipstore.withun.link/124021.png" alt="Twitter">
+    </a>
+</div>
+</div>
+</div>
+</header>
+    <div class="mt-16"><div class="flex items-center justify-center">
+  <div class="container">
+    <div class="bg-white w-full">
+      <div class="text-center">
+
+        <h2 class="heroprod text-4xl tracking-tight leading-10 text-gray-700 sm:text-5xl sm:leading-none md:text-6xl">
+          Flip Play
+        </h2>
+        <div class="m-3 flex">
+
+    <a href="https://flipstore.withun.link" class="w-1/3 text-center bg-white tracking-wide text-gray-800 border-2 border-blue-500 hover:border-blue-500 hover:bg-blue-500 hover:text-white py-2 px-2 inline-flex items-center shadow-md">
+<span class="mx-auto">Home</span>
+</a><a href="https://flipstore.withun.link/lab.php" class="w-1/3 text-center bg-white tracking-wide text-gray-800 border-2 border-blue-500 hover:border-blue-500 hover:bg-blue-500 hover:text-white py-2 px-2 inline-flex items-center  shadow-md">
+<span class="mx-auto">Lab</span>
+</a>
+    <a href="https://flipstore.withun.link/about" class="w-1/3 text-center bg-white tracking-wide text-gray-800 border-2 border-blue-500 hover:border-blue-500 hover:bg-blue-500 hover:text-white py-2 px-2 inline-flex items-center shadow-md">
+<span class="mx-auto">About</span>
+</a>
+</div>
+        <h3 class='text-xl md:text-3xl mt-10'>Application store for 4G Flip phones,<br>now for Exalt LTE.</h3>
+        <p class="text-md md:text-xl mt-10">We are offering application that can be used for newly released flip phones.<br>Download our official app in your flip phone and start enjoying!</p>
+      </div>
+      dgdgdgdg
+    </div>
+  </div>
+</div>
+</div>
+    <!-- Footer-->
+    <div class="bg-gray-100">
+   <div class="max-w-6xl m-auto text-gray-800 flex flex-wrap justify-center">
+      <div class="p-5 w-48 ">
+         <div class="text-xs uppercase text-gray-500 font-medium">Recommended</div>
+         <a class="my-3 block" href="https://Apps4Flip.com">Apps4Flip<span class="text-teal-600 text-xs p-1"></span></a><a class="my-3 block" href="https://github.com/sh7411usa/VirtualFlip">VirtualFlip <span class="text-teal-600 text-xs p-1"></span></a> 
+      </div>
+      <div class="p-5 w-48 ">
+         <div class="text-xs uppercase text-gray-500 font-medium">Download</div>
+         <a class="my-3 block" href="https://downapi.cafe.naver.com/v1.0/cafes/article/file/33af41eb-95cd-11eb-8894-0050568dd43a/download">App (한국어, 1.4)<span class="text-teal-600 text-xs p-1"></span></a><a class="my-3 block" href="/#">App (English, 1.4) <span class="text-teal-600 text-xs p-1"></span></a><a class="my-3 block" href="https://appetize.io/embed/4p9kuny8bx0hu7zkgp0qchuezc?device=nexus5&scale=75&orientation=portrait&osVersion=10.0&screenOnly=true&timezone=Asia%2FSeoul&androidPackageManager=true&centered=true">Online Simulator <span class="text-teal-600 text-xs p-1">New</span></a>
+      </div>
+
+   </div>
+</div>
+
+<div class="bg-gray-100 pt-2 ">
+   <div class="flex pb-5 px-3 m-auto pt-5 border-t text-gray-800 text-sm flex-col
+      md:flex-row max-w-6xl">
+      <div class="mt-2">© Copyright 2021 PaperCore. All rights reserved.</div>
+      <div class="md:flex-auto md:flex-row-reverse mt-2 flex-row flex">
+      </div>
+   </div>
+</div>
+</body>
+</html>
