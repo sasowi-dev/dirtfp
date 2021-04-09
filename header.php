520189771e4587891b13bee89934dc585048343b
PaperCore <redev@redevs.ga>
2021-04-09 22:59:04 +0900

    Update header.php

--- header.php
+++ header.php
@@ -1,16 +1,12 @@
-<header class="bg-white shadow-sm w-full z-10 fixed top-0 ">
+<header class="w-full z-10 top-0">
 <div class="max-auto  px-4 py-2 bg-white">
 <div class="flex justify-between">
 <a href="https://flipstore.withun.link/app">
 <div class="logo flex items-center mr-10">
-<img class="h-8" src="https://flipstore.withun.link/identity/Group%201.svg" alt="">
-<h1 class="text-gray-600 text-2xl" id="product-sans">FP Play</h1>
+<h1 class="text-black text-2xl" id="product-sans">FP Play</h1>
 </div>
 </a>
 <div class=" menu flex justify-end  items-center  flex-1 space-x-4">
-    <a href="https://flipstore.withun.link/twitter.php">
-<img class="h-9 rounded-full border border-gray-100 shadow-sm" src="https://flipstore.withun.link/124021.png" alt="Twitter">
-    </a>
 </div>
 </div>
 </div>
