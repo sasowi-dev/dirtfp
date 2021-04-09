c6dfc98ef7c70bdb1290460a37f2f8af2a663499
PaperCore <redev@redevs.ga>
2021-04-09 22:51:50 +0900

    Create index.php

---
+++ app/index.php
@@ -0,0 +1,32 @@
+<!DOCTYPE HTML>
+<html lang="ko">
+<head>
+<meta charset="utf-8">
+<?php include '../dependencies.php';?>
+  </head>
+  <body>
+<div class="absolute inset-0 bg-primary-gray-100 z-30">
+                    <div class="absolute flex justify-center left-0 right-0" style="top: 35%">
+                        <div class="relative">
+                            <div class="relative">
+                                <img src="https://i.imgur.com/l1696hT.png" class="w-full" style="max-width: 100px;">
+                            </div>
+                            <div class="absolute flex justify-center items-center inset-0 font-bold text-3xl text-blue-100 animate-pulse">
+                                FP
+                            </div>
+                        </div>
+                    </div>
+                    <div class="absolute flex justify-center left-0 right-0" style="bottom: 25%">
+                        <div>
+                            <img src="https://i.imgur.com/aNEOUL1.png" class="w-full" style="max-width: 180px;">
+                        </div>
+                    </div>
+                </div>
+    
+<script>
+      setTimeout(function(){
+       location.href = "applicationstore.php";
+    }, 1000)
+    </script>
+</body>
+</html>
