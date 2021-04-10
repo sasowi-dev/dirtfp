4c406a8d0c4d07ef55558287e8c6f6315b82c635
PaperCore <redev@redevs.ga>
2021-04-10 22:16:00 +0900

    Update and rename app/aboutpage.php to more-cdn/aboutpage.php

--- more-cdn/aboutpage.php
+++ more-cdn/aboutpage.php
@@ -41,36 +41,36 @@
 <div class="relative z-10 h-full" style="border-radius: 2rem;">
 <div class="relative w-full px-6">
 <div class="relative w-full flex flex-no-wrap justify-between items-center">
-<div class="text-lg font-semibold" data-ref="app-themes-text-title">Black Onyx</div>
+<div class="text-lg font-semibold" id="applicationid"></div>
 <div class="text-md font-semibold text-gray-300 flex items-center">
 <div>
-<img src="https://itest.nz/assets/img/galaxy-store/icon-rating.png" class="mr-1" style="width: 13px;">
+<img src="https://itest.nz/assets/img/galaxy-store/icon-rating.png" class="mr-1" id="rating" style="width: 13px;">
 </div>
 <div>
 5.0
 </div>
 </div>
 </div>
-<div class="relative w-full mb-4 text-gray-395">
+<div class="relative w-full mb-4 text-gray-395" id="vendor">
 by Samsung
 </div>
 </div>
 <div class="relative w-full">
 <div class="hidden" data-ref="app-themes-image-template">
 <div style="width: 40vw;" class="mr-1">
-<img src="https://itest.nz/assets/img/themes/black-onix/preview/icon-theme-1.jpg" class="rounded-2xl w-full border border-gray-150" style="width: 45vw;">
+<img id="screenshot1" class="rounded-2xl w-full border border-gray-150" style="width: 45vw;">
 </div>
 </div>
 <div class="relative flex w-full overflow-x-auto scrolling-touch pb-4">
 <div class="relative flex flex-no-wrap px-6 justify-center items-start" data-ref="app-themes-image-list">
 <div style="width: 40vw;" class="mr-1">
-<img src="https://itest.nz/assets/img/themes/black-onix/preview/icon-theme-2.jpg" class="rounded-2xl w-full border border-gray-150" style="width: 45vw;">
+<img id="screenshot2" class="rounded-2xl w-full border border-gray-150" style="width: 45vw;">
 </div>
 <div style="width: 40vw;" class="mr-1">
-<img src="https://itest.nz/assets/img/themes/black-onix/preview/icon-theme-3.jpg" class="rounded-2xl w-full border border-gray-150" style="width: 45vw;">
+<img id="screenshot3" class="rounded-2xl w-full border border-gray-150" style="width: 45vw;">
 </div>
 <div style="width: 40vw;" class="mr-1">
-<img src="https://itest.nz/assets/img/themes/black-onix/preview/icon-theme-1.jpg" class="rounded-2xl w-full border border-gray-150" style="width: 45vw;">
+<img id="screenshot4" class="rounded-2xl w-full border border-gray-150" style="width: 45vw;">
 </div>
 </div>
 </div>
@@ -78,9 +78,9 @@ by Samsung
 <div class="relative w-full px-6">
 <div class="relative w-full mt-2 mb-6">
 <div class="text-xl">
-Description
+설명
 </div>
-<div class="text-md text-gray-400 mt-2 leading-snug" data-ref="app-themes-text-description">There are 1000s of digital artists on the internet and with Galaxy Themes you can put them to work designing your phone how you like it.</div>
+<div class="text-md text-gray-400 mt-2 leading-snug" id="applicationinfo"></div>
 </div>
 </div>
 </div>
@@ -91,9 +91,9 @@ Description
 </div>
 <div class="absolute bottom-0 left-0 right-0 w-full bg-white text-center">
 <div class="relative w-full">
-<button class="text-xl font-semibold text-blue-500 py-4 w-full" data-ref="app-theme-button-apply">
+  <a id="applyapp" class="text-xl font-semibold text-blue-500 py-4 w-full">
 다운로드
-</button>
+  </a>
 </div>
 
 </div>
