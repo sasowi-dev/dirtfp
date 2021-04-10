ed65476e6f5d5fe9d2aacf43c0000ca7e646f665
PaperCore <redev@redevs.ga>
2021-04-10 18:53:52 +0900

    Create test.php

---
+++ about-cdn/test.php
@@ -0,0 +1,102 @@
+<!DOCTYPE HTML>
+<html lang="ko">
+<head>
+<meta charset="utf-8">
+<?php include '../dependencies.php';?>
+  </head>
+  <body>
+<?php include '../event.php';?>
+<div class="absolute top-0 left-0 right-0 bottom-0" style="min-width: 100vw; min-height: 100vh">
+<div class="absolute top-0 bottom-0 left-0 right-0">
+<div class="relative w-full h-full overflow-x-hidden">
+<div class="absolute left-0 right-0 overflow-hidden bg-white" style="top: 0px; height: calc(100% - 50px); border-radius: 2rem; border-top-left-radius: 0; border-top-right-radius: 0;">
+<div class="absolute top-0 left-0 right-0 bottom-0 overflow-y-auto scrolling-touch">
+<div class="relative w-full">
+<div class="sticky top-0 pb-3 z-20 bg-white">
+<div class="absolute w-full pointer-events-none" style="top: calc(100% - 20px); height: 50px;">
+<div class="absolute top-0 w-full">
+</div>
+</div>
+<div class="relative px-6">
+
+<div class="flex flex-no-wrap justify-between items-center">
+ <div class="flex flex-no-wrap items-center">
+<div class="">
+<a href="javascript:history.back()">
+<button class="" style="width: 30px; height: 40px; pointer-events: auto;" data-ref="app-theme-button-close">
+<svg xmlns="http://www.w3.org/2000/svg" width="8.633" height="14.438" viewBox="0 0 8.633 14.438" style="gpu">
+<path d="M767.8,760.341l-5.8,5.8,5.8,5.8" transform="translate(-760.58 -758.927)" fill="none" data-ref="dashboard-footer-button-stroke" stroke="currentColor" stroke-linecap="round" stroke-width="2"></path>
+</svg>
+</button>
+</a>
+</div>
+<div class="text-xl leading-none">
+</div>
+</div>
+<div class="flex gap-2 flex-no-wrap">
+</div>
+</div>
+</div>
+</div>
+<div class="relative z-10 h-full" style="border-radius: 2rem;">
+<div class="relative w-full px-6">
+<div class="relative w-full flex flex-no-wrap justify-between items-center">
+<div class="text-lg font-semibold" data-ref="app-themes-text-title">Black Onyx</div>
+<div class="text-md font-semibold text-gray-300 flex items-center">
+<div>
+<img src="https://itest.nz/assets/img/galaxy-store/icon-rating.png" class="mr-1" style="width: 13px;">
+</div>
+<div>
+5.0
+</div>
+</div>
+</div>
+<div class="relative w-full mb-4 text-gray-395">
+by Samsung
+</div>
+</div>
+<div class="relative w-full">
+<div class="hidden" data-ref="app-themes-image-template">
+<div style="width: 40vw;" class="mr-1">
+<img src="https://itest.nz/assets/img/themes/black-onix/preview/icon-theme-1.jpg" class="rounded-2xl w-full border border-gray-150" style="width: 45vw;">
+</div>
+</div>
+<div class="relative flex w-full overflow-x-auto scrolling-touch pb-4">
+<div class="relative flex flex-no-wrap px-6 justify-center items-start" data-ref="app-themes-image-list">
+<div style="width: 40vw;" class="mr-1">
+<img src="https://itest.nz/assets/img/themes/black-onix/preview/icon-theme-2.jpg" class="rounded-2xl w-full border border-gray-150" style="width: 45vw;">
+</div>
+<div style="width: 40vw;" class="mr-1">
+<img src="https://itest.nz/assets/img/themes/black-onix/preview/icon-theme-3.jpg" class="rounded-2xl w-full border border-gray-150" style="width: 45vw;">
+</div>
+<div style="width: 40vw;" class="mr-1">
+<img src="https://itest.nz/assets/img/themes/black-onix/preview/icon-theme-1.jpg" class="rounded-2xl w-full border border-gray-150" style="width: 45vw;">
+</div>
+</div>
+</div>
+</div>
+<div class="relative w-full px-6">
+<div class="relative w-full mt-2 mb-6">
+<div class="text-xl">
+Description
+</div>
+<div class="text-md text-gray-400 mt-2 leading-snug" data-ref="app-themes-text-description">There are 1000s of digital artists on the internet and with Galaxy Themes you can put them to work designing your phone how you like it.</div>
+</div>
+</div>
+</div>
+</div>
+</div>
+</div>
+</div>
+</div>
+<div class="absolute bottom-0 left-0 right-0 w-full bg-white text-center">
+<div class="relative w-full">
+<button class="text-xl font-semibold text-blue-500 py-4 w-full" data-ref="app-theme-button-apply">
+다운로드
+</button>
+</div>
+
+</div>
+</div>
+</body>
+</html>
