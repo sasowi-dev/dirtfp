b1d8e4713a56e0525f4518430842ce616ed0cfdb
PaperCore <redev@redevs.ga>
2021-04-10 22:49:56 +0900

    Create com.ss.squarehome2.php

---
+++ more-cdn/com.ss.squarehome2.php
@@ -0,0 +1,19 @@
+<?php include 'aboutpage.php';?>
+<script>
+
+  function metadatastd(appid, vendor, rate, information, sc1, sc2, sc3, sc4){
+  document.getElementById("applicationid").innerHTML = appid;
+  document.getElementById("vendor").innerHTML = vendor;
+  document.getElementById("rating").innerHTML = rate;
+  document.getElementById("applicationinfo").innerHTML = information;
+  document.getElementById("screenshot1").setAttribute("src", sc1);
+  document.getElementById("screenshot2").setAttribute("src", sc2);
+  document.getElementById("screenshot3").setAttribute("src", sc3);
+  document.getElementById("screenshot4").setAttribute("src", sc4);
+  }
+  function appdown(){
+  window.open("https://flipstore.withun.link/app-cdn/com.ss.squarehome2.apk","_self"); 
+  }
+  
+  metadatastd('스퀘어홈 런처 - 윈도우 스타일', 'ChYK the dev.', '4.6','* 이 앱에서는 기기 관리자 권한이 사용됩니다.<br>(안드로이드 버전이 9.0보다 낮을 경우, "화면 잠금" 런처 동작을 사용할 경우, 기기관리자 권한을 허용해야 합니다.)<br>* 이 앱에서는 접근성 서비스가 사용됩니다.<br>(일부 런처 동작 실행에서 필요할 경우만 요청됩니다.)<br><br>스퀘어홈은 윈도우 10과 같은 메트로 UI를 채용한 최고의 런처 애플리케이션입니다.<br>폰, 태블릿, TV박스 등 어떤 기기에서도 완벽하게 동작하며, 매우 사용하기 쉽고, 단순하고 아름답지만 강력합니다.<br><br>주요 기능:<br>- 페이지 내에서는 세로 스크롤, 페이지 간에는 가로 스크롤 지원<br>- 완벽한 메트로 UI 스타일과 태블릿 지원<br>- 아름다운 타일 효과<br>- 알림 메시지를 타일 위에 표시<br>- 똑똑한 앱서랍: 사용자의 앱 사용 패턴을 분석하여, 시간/위치/기기 상태에 따라 최선의 앱을 상단에 노출<br>- 연락처에 빠른 접근<br>- 풍부한 사용자 정의 옵션<br><br><br>선택적 사용 권한:<br>이 앱은 기능에 따라 다음 권한을 요구할 수 있으며, 원하지 않을 경우 모두 허용하지 않을 수 있습니다.<br><br>- [선택] 저장공간: 포토 슬라이드 위젯, 백업센터<br>- [선택] 전화: 연락처에서 길게 눌러 전화 걸기<br>- [선택] 주소록: 연락처 페이지, 라이브 연락처 위젯<br>- [선택] 카메라: 손전등 위젯<br>- [선택] 캘린더: 캘린더 위젯 이벤트 표시<br>- [선택] 메일 정보에 액세스: 타일에 미확인 G메일 개수 표시<br>----<br>개발자 연락처 :<br>* Please contact me by email instead of calling or text. Thank you.<br><br>+821026144726','https://play-lh.googleusercontent.com/dbye7qZbG3K8IPnaiAyRe1hN96xnnPgOkYPaRZLmHlq9vFuZ4U-TRfv6xOhjITQi7z0=w720-h310-rw','https://play-lh.googleusercontent.com/QJcZAm18LX87Y7noQodPKT9Il7ZPghBMXxx8NTdCennHjqJBjRrRNKYe8rOVWUIM3g=w720-h310-rw','https://play-lh.googleusercontent.com/k-ga1L6sE9f09C0ZnGcoir2Yh0JxFE_9RqWZYz2n7dlaF10Vui2RMp7SVNx4pOltap8=w720-h310-rw','https://play-lh.googleusercontent.com/Zlbl5Xfjmr5rA1KmWwgtY_eEIec1oe7DvzMBvbeUa4hoOSgA_jx39VYS_gatv6dQU3U=w720-h310-rw');
+ </script>
